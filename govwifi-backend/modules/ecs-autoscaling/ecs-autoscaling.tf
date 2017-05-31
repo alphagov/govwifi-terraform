12428c3c2a46d20e0ba8a2fd7c363bf255503e4d
Attila Foldesi <attila.foldesi@digital.cabinet-office.gov.uk>
2017-05-31 12:44:39 +0100

    Follow up manual changes to autoscaling group metrics.

--- govwifi-backend/modules/ecs-autoscaling/ecs-autoscaling.tf
+++ govwifi-backend/modules/ecs-autoscaling/ecs-autoscaling.tf
@@ -8,6 +8,16 @@ resource "aws_autoscaling_group" "ecs-cluster" {
   health_check_type         = "EC2"
   launch_configuration      = "${aws_launch_configuration.ecs.name}"
   health_check_grace_period = "${var.health_check_grace_period}"
+  enabled_metrics           = [
+    "GroupMinSize",
+    "GroupMaxSize",
+    "GroupDesiredCapacity",
+    "GroupInServiceInstances",
+    "GroupPendingInstances",
+    "GroupStandbyInstances",
+    "GroupTerminatingInstances",
+    "GroupTotalInstances",
+  ]
 
   lifecycle {
     create_before_destroy = true
