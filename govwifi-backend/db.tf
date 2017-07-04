b193de4075822732cf2c85ac8d12a274c1dfa57a
Attila Foldesi <attila.foldesi@digital.cabinet-office.gov.uk>
2017-07-04 13:39:23 +0100

    Add alarms to the DB metrics corresponding to CPU, memory and storage space.

--- govwifi-backend/db.tf
+++ govwifi-backend/db.tf
@@ -37,3 +37,60 @@ resource "aws_db_instance" "db" {
     Name = "wifi-${var.Env-Name}-db"
   }
 }
+
+resource "aws_cloudwatch_metric_alarm" "db_cpualarm" {
+  alarm_name          = "${var.Env-Name}-db-cpu-alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/RDS"
+  period              = "60"
+  statistic           = "Maximum"
+  threshold           = "80"
+
+  dimensions {
+    DBInstanceIdentifier = "${aws_db_instance.db.identifier}"
+  }
+
+  alarm_description  = "This metric monitors the cpu utilization of the DB"
+  alarm_actions      = ["${var.critical-notifications-arn}"]
+  treat_missing_data = "breaching"
+}
+
+resource "aws_cloudwatch_metric_alarm" "db_memoryalarm" {
+  alarm_name          = "${var.Env-Name}-db-memory-alarm"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "FreeableMemory"
+  namespace           = "AWS/RDS"
+  period              = "60"
+  statistic           = "Minimum"
+  threshold           = "524288000"
+
+  dimensions {
+    DBInstanceIdentifier = "${aws_db_instance.db.identifier}"
+  }
+
+  alarm_description  = "This metric monitors the freeable memory available for the DB."
+  alarm_actions      = ["${var.critical-notifications-arn}"]
+  treat_missing_data = "breaching"
+}
+
+resource "aws_cloudwatch_metric_alarm" "db_storagealarm" {
+  alarm_name          = "${var.Env-Name}-db-storage-alarm"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "FreeStorageSpace"
+  namespace           = "AWS/RDS"
+  period              = "60"
+  statistic           = "Minimum"
+  threshold           = "21474836480"
+
+  dimensions {
+    DBInstanceIdentifier = "${aws_db_instance.db.identifier}"
+  }
+
+  alarm_description  = "This metric monitors the storage space available for the DB."
+  alarm_actions      = ["${var.critical-notifications-arn}"]
+  treat_missing_data = "breaching"
+}
