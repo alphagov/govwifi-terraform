928dc112945aa129ee7ad624c722de37be37ab78
Attila Foldesi <attila.foldesi@digital.cabinet-office.gov.uk>
2018-03-07 15:11:32 +0000

    Activate encryption and replication for tfstate buckets, split out accesslogs configuration.

---
+++ terraform-state/accesslogs.tf
@@ -0,0 +1,118 @@
+# --------------------------------------------------------------
+# Access logs
+# --------------------------------------------------------------
+
+resource "aws_iam_role" "accesslogs-replication" {
+  name = "${lower(var.product-name)}-${var.Env-Name}-${lower(var.aws-region-name)}-accesslogs-replication-role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "s3.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_policy" "accesslogs-replication" {
+  name = "${lower(var.product-name)}-${lower(var.Env-Name)}-${lower(var.aws-region-name)}-accesslogs-replication-policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:GetReplicationConfiguration",
+        "s3:ListBucket"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "${aws_s3_bucket.accesslogs-bucket.arn}"
+      ]
+    },
+    {
+      "Action": [
+        "s3:GetObjectVersion",
+        "s3:GetObjectVersionAcl"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "${aws_s3_bucket.accesslogs-bucket.arn}/*"
+      ]
+    },
+    {
+      "Action": [
+         "s3:ReplicateObject",
+         "s3:ReplicateDelete"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::${lower(var.product-name)}-${var.Env-Name}-${lower(var.backup-region-name)}-accesslogs/*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "accesslogs-replication" {
+  name       = "${lower(var.product-name)}-${lower(var.Env-Name)}-${lower(var.aws-region-name)}-accesslogs-replication"
+  roles      = ["${aws_iam_role.accesslogs-replication.name}"]
+  policy_arn = "${aws_iam_policy.accesslogs-replication.arn}"
+}
+
+resource "aws_s3_bucket" "accesslogs-bucket" {
+  bucket = "${lower(var.product-name)}-${var.Env-Name}-${lower(var.aws-region-name)}-accesslogs"
+  region = "${var.aws-region}"
+  acl    = "log-delivery-write"
+
+  tags {
+    Region      = "${title(var.aws-region-name)}"
+    Product     = "${var.product-name}"
+    Environment = "${title(var.Env-Name)}"
+    Category    = "Accesslogs"
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    id = "${lower(var.product-name)}-${lower(var.Env-Name)}-${lower(var.aws-region-name)}-accesslogs-lifecycle"
+    enabled = true
+
+    transition {
+      days          = "${var.accesslogs-glacier-transition-days}"
+      storage_class = "GLACIER"
+    }
+
+    expiration {
+      days          = "${var.accesslogs-expiration-days}"
+    }
+  }
+
+  replication_configuration {
+    role = "${aws_iam_role.accesslogs-replication.arn}"
+
+    rules {
+      # ID is necessary to prevent continuous change issue
+      id     = "${lower(var.aws-region-name)}-to-${lower(var.backup-region-name)}-accesslogs-backup"
+      prefix = "${lower(var.aws-region-name)}-accesslogs-backup"
+      status = "Enabled"
+
+      destination {
+        bucket        = "arn:aws:s3:::${lower(var.product-name)}-${lower(var.Env-Name)}-${lower(var.backup-region-name)}-accesslogs"
+        storage_class = "STANDARD"
+      }
+    }
+  }
+
+
+}
--- terraform-state/tfstate.tf
+++ terraform-state/tfstate.tf
@@ -66,6 +66,23 @@ resource "aws_iam_policy_attachment" "tfstate-replication" {
   policy_arn = "${aws_iam_policy.tfstate-replication.arn}"
 }
 
+resource "aws_kms_key" "tfstate-key" {
+  description             = "KMS key for the encryption of tfstate buckets."
+  deletion_window_in_days = 10
+  is_enabled              = true
+  tags {
+    Region      = "${title(var.aws-region-name)}"
+    Product     = "${var.product-name}"
+    Environment = "${title(var.Env-Name)}"
+    Category    = "TFstate"
+  }
+}
+
+resource "aws_kms_alias" "tfstate-key-alias" {
+  name          = "alias/${lower(var.product-name)}-${lower(var.Env-Name)}-${lower(var.aws-region-name)}-tfstate-key"
+  target_key_id = "${aws_kms_key.tfstate-key.key_id}"
+}
+
 resource "aws_s3_bucket" "state-bucket" {
   bucket = "${lower(var.product-name)}-${lower(var.Env-Name)}-${lower(var.aws-region-name)}-tfstate"
   region = "${var.aws-region}"
@@ -116,124 +133,19 @@ EOF
 
       destination {
         bucket        = "arn:aws:s3:::${lower(var.product-name)}-${lower(var.Env-Name)}-${lower(var.backup-region-name)}-tfstate"
-        storage_class = "STANDARD"
+        storage_class = "STANDARD_IA"
+      }
+    }
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${aws_kms_key.tfstate-key.arn}"
+        sse_algorithm     = "aws:kms"
       }
     }
   }
 }
 
-# --------------------------------------------------------------
 
-resource "aws_iam_role" "accesslogs-replication" {
-  name = "${lower(var.product-name)}-${var.Env-Name}-${lower(var.aws-region-name)}-accesslogs-replication-role"
-
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "s3.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-POLICY
-}
-
-resource "aws_iam_policy" "accesslogs-replication" {
-  name = "${lower(var.product-name)}-${var.Env-Name}-${lower(var.aws-region-name)}-accesslogs-replication-policy"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "s3:GetReplicationConfiguration",
-        "s3:ListBucket"
-      ],
-      "Effect": "Allow",
-      "Resource": [
-        "${aws_s3_bucket.accesslogs-bucket.arn}"
-      ]
-    },
-    {
-      "Action": [
-        "s3:GetObjectVersion",
-        "s3:GetObjectVersionAcl"
-      ],
-      "Effect": "Allow",
-      "Resource": [
-        "${aws_s3_bucket.accesslogs-bucket.arn}/*"
-      ]
-    },
-    {
-      "Action": [
-        "s3:ReplicateObject",
-        "s3:ReplicateDelete"
-      ],
-      "Effect": "Allow",
-      "Resource": "arn:aws:s3:::${lower(var.product-name)}-${var.Env-Name}-${lower(var.backup-region-name)}-accesslogs/*"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_policy_attachment" "accesslogs-replication" {
-  name       = "${lower(var.product-name)}-${lower(var.Env-Name)}-${lower(var.aws-region-name)}-accesslogs-replication"
-  roles      = ["${aws_iam_role.accesslogs-replication.name}"]
-  policy_arn = "${aws_iam_policy.accesslogs-replication.arn}"
-}
-
-resource "aws_s3_bucket" "accesslogs-bucket" {
-  bucket = "${lower(var.product-name)}-${var.Env-Name}-${lower(var.aws-region-name)}-accesslogs"
-  region = "${var.aws-region}"
-  acl    = "log-delivery-write"
-
-  versioning {
-    enabled = true
-  }
-
-  tags {
-    Region      = "${title(var.aws-region-name)}"
-    Product     = "${var.product-name}"
-    Environment = "${title(var.Env-Name)}"
-    Category    = "Accesslogs"
-  }
-
-
-  lifecycle_rule {
-    id = "${lower(var.product-name)}-${lower(var.Env-Name)}-${lower(var.aws-region-name)}-accesslogs-lifecycle"
-    enabled = true
-
-    transition {
-      days          = "${var.accesslogs-glacier-transition-days}"
-      storage_class = "GLACIER"
-    }
-
-    expiration {
-      days          = "${var.accesslogs-expiration-days}"
-    }
-  }
-
-  replication_configuration {
-    role = "${aws_iam_role.accesslogs-replication.arn}"
-
-    rules {
-      # ID is necessary to prevent continuous change issue
-      id     = "${lower(var.aws-region-name)}-to-${lower(var.backup-region-name)}-accesslogs-backup"
-      prefix = "${lower(var.aws-region-name)}-accesslogs-backup"
-      status = "Enabled"
-
-      destination {
-        bucket        = "arn:aws:s3:::${lower(var.product-name)}-${lower(var.Env-Name)}-${lower(var.backup-region-name)}-accesslogs"
-        storage_class = "STANDARD"
-      }
-    }
-  }
-}
