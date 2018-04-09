26aeea9ad2b614f4a8c7fb3c353b75504c18d74b
Attila Foldesi <attila.foldesi@digital.cabinet-office.gov.uk>
2018-04-09 15:30:35 +0100

    Remove specific access key requirement, and add the neccessary privileges via the instance profile.

--- govwifi-backend/ecs-autoscaling.tf
+++ govwifi-backend/ecs-autoscaling.tf
@@ -17,6 +17,4 @@ module "ecs-autoscaling" {
   critical-notifications-arn = "${var.critical-notifications-arn}"
   capacity-notifications-arn = "${var.capacity-notifications-arn}"
   users                      = "${var.users}"
-  aws-cloudwatch-access-key  = "${var.aws-cloudwatch-access-key}"
-  aws-cloudwatch-secret-key  = "${var.aws-cloudwatch-secret-key}"
 }
