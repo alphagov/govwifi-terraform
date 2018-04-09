d1df7682c0bb02e7c3d6481479f85283563aeb3c
Emile <emile.swarts123@gmail.com>
2018-04-09 14:41:33 +0100

    Use monitoring scripts directory

--- govwifi-backend/modules/ecs-autoscaling/launch-configuration.tf
+++ govwifi-backend/modules/ecs-autoscaling/launch-configuration.tf
@@ -36,12 +36,12 @@ Content-Type: text/x-shellscript; charset="us-ascii"
 #!/bin/bash
 sudo yum install perl-Switch perl-DateTime perl-Sys-Syslog perl-LWP-Protocol-https -y
 sudo yum -y install perl-Digest-SHA perl-URI perl-libwww-perl perl-MIME-tools perl-Crypt-SSLeay perl-XML-LibXML
-mkdir /home/ec2-user/scripts
-cd /home/ec2-user/scripts
+mkdir -p /home/ec2-user/scripts
 curl https://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.2.zip -O
 unzip CloudWatchMonitoringScripts-1.2.2.zip
 rm CloudWatchMonitoringScripts-1.2.2.zip
-cd aws-scripts-mon
+mv CloudWatchMonitoringScripts-1.2.2 /home/ec2-user/scripts/mon
+cd /home/ec2-user/scripts/mon
 
 cat <<EOF > ./awscreds.conf
 AWSAccessKeyId=${var.aws-cloudwatch-access-key}
