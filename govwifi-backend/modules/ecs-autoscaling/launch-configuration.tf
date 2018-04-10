215a62a2aa487fa3283bfff3147675d800b55d49
Emile <emile.swarts123@gmail.com>
2018-04-10 14:53:26 +0100

    Script To Enable Cloudwatch Memory Logging

--- govwifi-backend/modules/ecs-autoscaling/launch-configuration.tf
+++ govwifi-backend/modules/ecs-autoscaling/launch-configuration.tf
@@ -21,6 +21,20 @@ repo_update: true
 MIME-Version: 1.0
 Content-Type: text/x-shellscript; charset="us-ascii"
 #!/bin/bash
+sudo yum install perl-Switch perl-DateTime perl-Sys-Syslog perl-LWP-Protocol-https -y
+sudo yum -y install perl-Digest-SHA perl-URI perl-libwww-perl perl-MIME-tools perl-Crypt-SSLeay perl-XML-LibXML unzip curl
+mkdir -p /home/ec2-user/scripts
+cd /home/ec2-user/scripts
+curl https://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.2.zip -O
+unzip CloudWatchMonitoringScripts-1.2.2.zip
+rm CloudWatchMonitoringScripts-1.2.2.zip
+mv aws-scripts-mon /home/ec2-user/scripts/mon
+cd /home/ec2-user/scripts/mon
+
+--==BOUNDARY==
+MIME-Version: 1.0
+Content-Type: text/x-shellscript; charset="us-ascii"
+#!/bin/bash
 # Set up daily security updates
 # Stagger restart time based on local IP (assigned randomly)
 # Restarting all backends before the frontends start their staggered nighly restarts
@@ -45,6 +59,7 @@ HOME=/
 # |  |  |  |  |
 # *  *  *  *  * user-name command to be executed
 
+* * * * * root /home/ec2-user/scripts/mon/mon-put-instance-data.pl --mem-used --from-cron --mem-avail --swap-used --mem-used-incl-cache-buff
 42 * * * * root   run-parts /etc/cron.hourly
 $MINS 0 * * * root   run-parts /etc/cron.daily
 
