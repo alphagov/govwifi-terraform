589d61ef7f46d73a5a09b1dec4e1e71d140ecb19
Attila Foldesi <attila.foldesi@digital.cabinet-office.gov.uk>
2018-05-16 09:30:38 +0100

    Make sure that the session deletion runs for one hour only.

--- govwifi-lambda/cloudwatch.tf
+++ govwifi-lambda/cloudwatch.tf
@@ -23,7 +23,7 @@ resource "aws_lambda_permission" "allow_cloudwatch_to_call_delete_users" {
 resource "aws_cloudwatch_event_rule" "delete_sessions_frequency" {
   name                = "delete-sessions-frequency-${var.Env-Name}-${lower(var.aws-region-name)}"
   description         = "Triggers every 5 minutes between 2200-2300 UTC"
-  schedule_expression = "cron(0/5 22-23 * * ? *)"
+  schedule_expression = "cron(0/5 22 * * ? *)"
   is_enabled          = "${var.enable-session-del-cron}"
 }
 
