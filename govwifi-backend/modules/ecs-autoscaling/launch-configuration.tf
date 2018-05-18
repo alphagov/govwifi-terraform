fc23bbcbba8a05f5351b41d278f9bc4a3a3c82a6
Attila Foldesi <attila.foldesi@digital.cabinet-office.gov.uk>
2018-05-18 15:00:07 +0100

    Remove superfluous mon configuration.

--- govwifi-backend/modules/ecs-autoscaling/launch-configuration.tf
+++ govwifi-backend/modules/ecs-autoscaling/launch-configuration.tf
@@ -34,20 +34,6 @@ mv aws-scripts-mon /home/ec2-user/scripts/mon
 MIME-Version: 1.0
 Content-Type: text/x-shellscript; charset="us-ascii"
 #!/bin/bash
-sudo yum install perl-Switch perl-DateTime perl-Sys-Syslog perl-LWP-Protocol-https -y
-sudo yum -y install perl-Digest-SHA perl-URI perl-libwww-perl perl-MIME-tools perl-Crypt-SSLeay perl-XML-LibXML unzip
-mkdir -p /home/ec2-user/scripts
-cd /home/ec2-user/scripts
-curl https://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.2.zip -O
-unzip CloudWatchMonitoringScripts-1.2.2.zip
-rm CloudWatchMonitoringScripts-1.2.2.zip
-mv aws-scripts-mon /home/ec2-user/scripts/mon
-cd /home/ec2-user/scripts/mon
-
---==BOUNDARY==
-MIME-Version: 1.0
-Content-Type: text/x-shellscript; charset="us-ascii"
-#!/bin/bash
 # Set up daily security updates
 # Stagger restart time based on local IP (assigned randomly)
 # Restarting all backends before the frontends start their staggered nighly restarts
