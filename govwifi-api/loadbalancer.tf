52503853cc5450896cbc75b635e8f26cb8336705
Emile <emile.swarts123@gmail.com>
2018-05-31 17:21:17 +0100

    Use new ALB

--- govwifi-api/loadbalancer.tf
+++ govwifi-api/loadbalancer.tf
@@ -1,39 +1,69 @@
-resource "aws_elb" "api-elb" {
+resource "aws_lb" "api-alb" {
+  name            = "api-alb-${var.Env-Name}"
+  internal        = true
   count           = "${var.backend-elb-count}"
-  name            = "api-elb-${var.Env-Name}"
   subnets         = ["${var.subnet-ids}"]
   security_groups = ["${var.elb-sg-list}"]
-
-  listener {
-    instance_port      = "${var.clients-instance-port}" #8080
-    instance_protocol  = "http"
-    lb_port            = "${var.clients-elb-port}" #8443
-    lb_protocol        = "https"
-    ssl_certificate_id = "${var.elb-ssl-cert-arn}"
-  }
-
-  listener {
-    instance_port      = "${var.auth-instance-port}"
-    instance_protocol  = "http"
-    lb_port            = "${var.auth-elb-port}"
-    lb_protocol        = "https"
-    ssl_certificate_id = "${var.elb-ssl-cert-arn}"
-  }
-
-  health_check { #CRAP
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 3
-    target              = "HTTP:8080/"
-    interval            = 30
-  }
-
-  cross_zone_load_balancing   = true
-  idle_timeout                = 2
-  connection_draining         = true
-  connection_draining_timeout = 30
+  load_balancer_type = "application"
 
   tags {
-    Name = "api-elb-${var.Env-Name}"
+    Name = "api-alb-${var.Env-Name}"
   }
+}
+
+resource "aws_alb_listener" "alb_listener" {
+  load_balancer_arn = "${aws_lb.api-alb.arn}"
+  port              = "8443"
+  protocol          = "HTTPS"
+  certificate_arn   = "${var.elb-ssl-cert-arn}"
+  ssl_policy        = "ELBSecurityPolicy-2015-05"
+
+  default_action {
+    target_group_arn = "${aws_alb_target_group.alb_target.arn}"
+    type             = "forward"
+  }
+}
+
+resource "aws_alb_listener_rule" "static" {
+  depends_on   = ["aws_alb_target_group.alb_target_group"]
+  listener_arn = "${aws_alb_listener.alb_listener.arn}"
+  priority     = 1
+
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_alb_target_group.alb_target_group.id}"
+  }
+  condition {
+    field  = "path-pattern"
+    values = ["/authentication/*"]
+  }
+}
+
+resource "aws_alb_target_group" "alb_target_group" {
+  name     = "api-lb-tg-${var.Env-Name}"
+  port     = "8080"
+  protocol = "HTTP"
+  vpc_id   = "${module.backend.backend-vpc-id}"
+  tags {
+    name = "${var.target_group_name}"
+    Name = "api-alb-tg-${var.Env-Name}"
+  }
+
+  stickiness {
+    enabled         = false
+  }
+
+  health_check {
+    healthy_threshold   = 3
+    unhealthy_threshold = 10
+    timeout             = 5
+    interval            = 10
+    path                = "/health"
+    port                = "8080"
+  }
+}
+
+resource "aws_autoscaling_attachment" "svc_asg_external2" {
+  alb_target_group_arn   = "${aws_alb_target_group.alb_target_group.arn}"
+  autoscaling_group_name = "${aws_autoscaling_group.api-asg.id}"
 }
