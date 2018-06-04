8713dc39d73066dd02c7eb219a867bdcd17c66d4
Emile <emile.swarts123@gmail.com>
2018-06-04 11:30:04 +0100

    New infrastructure for Authentication API

---
+++ govwifi-api/elb.tf
@@ -0,0 +1,64 @@
+resource "aws_lb" "api-alb" {
+  name            = "api-alb-${var.Env-Name}"
+  internal        = true
+  count           = "${var.backend-elb-count}"
+  subnets         = ["${var.subnet-ids}"]
+  security_groups = ["${var.elb-sg-list}"]
+  load_balancer_type = "application"
+
+  tags {
+    Name = "api-alb-${var.Env-Name}"
+  }
+}
+
+resource "aws_alb_listener" "alb_listener" {
+  load_balancer_arn = "${aws_lb.api-alb.arn}"
+  port              = "8443"
+  protocol          = "HTTPS"
+  certificate_arn   = "${var.elb-ssl-cert-arn}"
+  ssl_policy        = "ELBSecurityPolicy-2015-05"
+
+  default_action {
+    target_group_arn = "${aws_alb_target_group.alb_target_group.arn}"
+    type             = "forward"
+  }
+}
+
+resource "aws_alb_listener_rule" "static" {
+  depends_on   = ["aws_alb_target_group.alb_target_group"]
+  listener_arn = "${aws_alb_listener.alb_listener.arn}"
+  priority     = 1
+
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_alb_target_group.alb_target_group.id}"
+  }
+  condition {
+    field  = "path-pattern"
+    values = ["/authentication/*"]
+  }
+}
+
+resource "aws_alb_target_group" "alb_target_group" {
+  name     = "api-lb-tg-${var.Env-Name}"
+  port     = "8080"
+  protocol = "HTTP"
+  vpc_id   = "${var.vpc-id}"
+  tags {
+    Name = "api-alb-tg-${var.Env-Name}"
+  }
+
+  health_check {
+    healthy_threshold   = 3
+    unhealthy_threshold = 10
+    timeout             = 5
+    interval            = 10
+    path                = "/health"
+    port                = "8080"
+  }
+}
+
+resource "aws_autoscaling_attachment" "svc_asg_external2" {
+  alb_target_group_arn   = "${aws_alb_target_group.alb_target_group.arn}"
+  autoscaling_group_name = "${aws_autoscaling_group.api-asg.id}"
+}
