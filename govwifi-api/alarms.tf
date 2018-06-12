501d0bd9a08e973cb4acc71dc8ced21576f28bfa
Emile <emile.swarts123@gmail.com>
2018-06-12 09:26:05 +0100

    Remove memory logging

--- govwifi-api/alarms.tf
+++ govwifi-api/alarms.tf
@@ -14,8 +14,8 @@ resource "aws_cloudwatch_metric_alarm" "ec2-api-cpu-alarm-low" {
 
   alarm_description  = "This alarm tells EC2 to scale in based on low CPU usage"
   alarm_actions      = [
-    "${aws_autoscaling_policy.api-ec2-scale-down-policy.arn}"
-    /* "${var.critical-notifications-arn}" */
+    "${aws_autoscaling_policy.api-ec2-scale-down-policy.arn}",
+    "${var.critical-notifications-arn}"
   ]
 
   treat_missing_data = "breaching"
@@ -37,9 +37,10 @@ resource "aws_cloudwatch_metric_alarm" "ec2-api-cpu-alarm-high" {
 
   alarm_description  = "This alarm tells EC2 to scale up based on high CPU usage"
   alarm_actions      = [
-    "${aws_autoscaling_policy.api-ec2-scale-up-policy.arn}"
-    /* "${var.critical-notifications-arn}" */
+    "${aws_autoscaling_policy.api-ec2-scale-up-policy.arn}",
+    "${var.critical-notifications-arn}"
   ]
+
   treat_missing_data = "breaching"
 }
 
@@ -60,9 +61,10 @@ resource "aws_cloudwatch_metric_alarm" "auth-ecs-cpu-alarm-high" {
 
   alarm_description  = "This alarm tells ECS to scale up based on high CPU"
   alarm_actions      = [
-    "${aws_appautoscaling_policy.ecs-policy-up.arn}"
-    /* "${var.critical-notifications-arn}" */
+    "${aws_appautoscaling_policy.ecs-policy-up.arn}",
+    "${var.critical-notifications-arn}"
   ]
+
   treat_missing_data = "breaching"
 }
 
@@ -83,51 +85,9 @@ resource "aws_cloudwatch_metric_alarm" "auth-ecs-cpu-alarm-low" {
 
   alarm_description  = "This alarm tells ECS to scale in based on low CPU usage"
   alarm_actions      = [
-    "${aws_appautoscaling_policy.ecs-policy-down.arn}"
-    /* "${var.critical-notifications-arn}" */
+    "${aws_appautoscaling_policy.ecs-policy-down.arn}",
+    "${var.critical-notifications-arn}"
   ]
-}
 
-resource "aws_cloudwatch_metric_alarm" "ec2-api-memory-alarm-high" {
-  alarm_name          = "${var.Env-Name}-ec2-api-memory-alarm-high"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "1"
-  metric_name         = "MemoryUtilization"
-  namespace           = "AWS/EC2"
-  period              = "300"
-  statistic           = "Average"
-  threshold           = "70"
-
-  dimensions {
-    AutoScalingGroupName = "${aws_autoscaling_group.api-asg.name}"
-  }
-
-  alarm_description  = "This alarm tells EC2 to scale up based on high memory usage"
-  alarm_actions      = [
-    "${aws_autoscaling_policy.api-ec2-scale-up-policy.arn}"
-    /* "${var.critical-notifications-arn}" */
-  ]
   treat_missing_data = "breaching"
-}
-
-resource "aws_cloudwatch_metric_alarm" "ec2-api-memory-alarm-low" {
-  alarm_name          = "${var.Env-Name}-ec2-api-memory-alarm-low"
-  comparison_operator = "LessThanOrEqualToThreshold"
-  evaluation_periods  = "1"
-  metric_name         = "MemoryUtilization"
-  namespace           = "AWS/EC2"
-  period              = "300"
-  statistic           = "Average"
-  threshold           = "30"
-
-  dimensions {
-    AutoScalingGroupName = "${aws_autoscaling_group.api-asg.name}"
-  }
-
-  alarm_description  = "This alarm tells EC2 to scale in based on low memory usage"
-  alarm_actions      = [
-    "${aws_autoscaling_policy.api-ec2-scale-down-policy.arn}"
-    /* "${var.critical-notifications-arn}" */
-  ]
-  treat_missing_data = "notBreaching"
 }
