640c59bb70da3f1db148b5820574a32d4cb7959e
Emile <emile.swarts123@gmail.com>
2018-07-31 10:15:14 +0100

    WIP logging API scheduled task

--- govwifi-api/cloudwatch-events.tf
+++ govwifi-api/cloudwatch-events.tf
@@ -1,13 +1,13 @@
 resource "aws_cloudwatch_event_rule" "daily_statistics_event" {
   name                = "${var.Env-Name}-daily-statistics-frequency"
   description         = "Triggers daily 0600 UTC"
-  schedule_expression = "cron(0 6 * * ? *)"
+  schedule_expression = "cron(37 * * * ? *)"
   is_enabled          = false
 }
 
 resource "aws_cloudwatch_event_rule" "weekly_statistics_event" {
   name                = "${var.Env-Name}-weekly-statistics-frequency"
   description         = "Triggers every SUN 0630 UTC"
-  schedule_expression = "cron(30 6 ? * SUN *)"
+  schedule_expression = "cron(37 * * * ? *)"
   is_enabled          = false
 }
