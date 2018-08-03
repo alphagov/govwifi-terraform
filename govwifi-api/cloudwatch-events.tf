9515ad980a06582a538e55a79f43781f56abb636
Emile Swarts <emile@madetech.com>
2018-08-03 11:08:10 +0100

    Update descriptions and enable scheduled tasks (#105)
    Fix the descriptions of the scheduled tasks to reflect when they will
    run.  Also enable these scheduled tasks.

--- govwifi-api/cloudwatch-events.tf
+++ govwifi-api/cloudwatch-events.tf
@@ -1,20 +1,20 @@
 resource "aws_cloudwatch_event_rule" "daily_statistics_event" {
   name                = "${var.Env-Name}-daily-statistics-frequency"
-  description         = "Triggers daily 0600 UTC"
+  description         = "Triggers daily 0625 UTC"
   schedule_expression = "cron(25 6 * * ? *)"
   is_enabled          = true
 }
 
 resource "aws_cloudwatch_event_rule" "weekly_statistics_event" {
   name                = "${var.Env-Name}-weekly-statistics-frequency"
-  description         = "Triggers every SUN 0630 UTC"
-  schedule_expression = "cron(47 6 * * 7 *)"
-  is_enabled          = false
+  description         = "Triggers every SUN 0647 UTC"
+  schedule_expression = "cron(47 6 ? * 7 *)"
+  is_enabled          = true
 }
 
 resource "aws_cloudwatch_event_rule" "monthly_statistics_event" {
   name                = "${var.Env-Name}-monthly-statistics-frequency"
-  description         = "Triggers every SUN 0630 UTC"
+  description         = "Triggers on the first of each month at 0652 UTC"
   schedule_expression = "cron(52 6 1 * ? *)"
-  is_enabled          = false
+  is_enabled          = true
 }
