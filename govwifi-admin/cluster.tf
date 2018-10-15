efb558cc4ef49f1a794d2b0258d41d3d6f546a8c
Jakub Naliwajek <naliwajek@gmail.com>
2018-10-15 11:53:51 +0100

    Add S3 bucket for MOUs in Admin

--- govwifi-admin/cluster.tf
+++ govwifi-admin/cluster.tf
@@ -83,6 +83,9 @@ resource "aws_ecs_task_definition" "admin-task" {
           "name": "SENTRY_DSN",
           "value": "${var.sentry-dsn}"
         },{
+          "name": "S3_MOU_BUCKET",
+          "value": "govwifi-${var.rack-env}-admin-mou"
+        },{
           "name": "S3_PUBLISHED_LOCATIONS_IPS_BUCKET",
           "value": "govwifi-${var.rack-env}-admin"
         },{
