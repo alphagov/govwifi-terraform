189db91738a0131d300402a75992057a28ad7a28
Emile <emile.swarts123@gmail.com>
2018-10-24 16:54:22 +0100

    Remove unneeded output from frontend module

--- govwifi-frontend/outputs.tf
+++ govwifi-frontend/outputs.tf
@@ -5,7 +5,3 @@ output "frontend-vpc-id" {
 output "frontend-subnet-id" {
   value = "${aws_subnet.wifi-frontend-subnet.*.id}"
 }
-
-output "frontend-log-group-name" {
-  value = "${aws_cloudwatch_log_group.frontend-log-group.name}"
-}
