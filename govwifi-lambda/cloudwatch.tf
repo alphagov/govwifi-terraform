c0aaaaeea7605da0edf520b86016336075cf9030
Emile Swarts <emile@madetech.com>
2018-12-03 15:43:45 +0000

    Decommission Session Deletion Lambda (#160)
    This is now an ECS scheduled task

--- govwifi-lambda/cloudwatch.tf
+++ govwifi-lambda/cloudwatch.tf
@@ -18,25 +18,3 @@ resource "aws_lambda_permission" "allow_cloudwatch_to_call_delete_users" {
   principal     = "events.amazonaws.com"
   source_arn    = "${aws_cloudwatch_event_rule.every_day_at_midnight.arn}"
 }
-
-
-resource "aws_cloudwatch_event_rule" "delete_sessions_frequency" {
-  name                = "delete-sessions-frequency-${var.Env-Name}-${lower(var.aws-region-name)}"
-  description         = "Triggers every 5 minutes between 2200-2300 UTC"
-  schedule_expression = "cron(0/5 22 * * ? *)"
-  is_enabled          = "${var.enable-session-del-cron}"
-}
-
-resource "aws_cloudwatch_event_target" "delete_sessions_every_day" {
-  rule      = "${aws_cloudwatch_event_rule.delete_sessions_frequency.name}"
-  target_id = "session_deletion_${var.Env-Name}-${lower(var.aws-region-name)}"
-  arn       = "${aws_lambda_function.session_deletion.arn}"
-}
-
-resource "aws_lambda_permission" "allow_cloudwatch_to_call_delete_sessions" {
-  statement_id  = "AllowExecutionFromCloudWatch"
-  action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.session_deletion.function_name}"
-  principal     = "events.amazonaws.com"
-  source_arn    = "${aws_cloudwatch_event_rule.delete_sessions_frequency.arn}"
-}
