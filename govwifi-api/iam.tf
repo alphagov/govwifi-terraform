530b9ed77121724891434acef7d85863090153a3
Mark Sta Ana <booyaabooyaabooyaa@gmail.com>
2019-01-14 17:06:17 +0000

    Move from ECS to Fargate for the Authorisation API (#174)
    Authentication is the last remaining API running on EC2, this converts
    it to use Fargate.
    
    Also promote some of the IAM roles into their own file since they are
    shared between a few APIs

---
+++ govwifi-api/iam.tf
@@ -0,0 +1,20 @@
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole-${var.rack-env}-${var.aws-region-name}"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
--- govwifi-api/logging-api-cluster.tf
+++ govwifi-api/logging-api-cluster.tf
@@ -211,26 +211,3 @@ resource "aws_iam_role_policy" "logging-api-task-policy" {
 }
 EOF
 }
-
-resource "aws_iam_role" "ecsTaskExecutionRole" {
-  count              = "${var.iam-count}"
-  name               = "ecsTaskExecutionRole-${var.rack-env}"
-  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
-}
-
-data "aws_iam_policy_document" "assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
-  count      = "${var.iam-count}"
-  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
