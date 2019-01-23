5322ce2b8c5e216ba9a144996f0068a7aed0fcc1
Anthony King <anthony@madetech.com>
2019-01-23 12:07:21 +0000

    add s3 bucket for certificate store

---
+++ govwifi-frontend/certs.tf
@@ -0,0 +1,38 @@
+resource "aws_s3_bucket" "frontend-cert-bucket" {
+  count         = 1
+  bucket        = "govwifi-${var.rack-env}-frontend-cert"
+  acl           = "private"
+
+  tags {
+    Name        = "${title(var.Env-Name)} Frontend certs"
+    Region      = "${title(var.aws-region-name)}"
+    Environment = "${title(var.rack-env)}"
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "AES256"
+      }
+    }
+  }
+}
+
+data "aws_iam_policy_document" "frontend-cert-bucket-policy-document" {
+  statement {
+    actions = [
+      "s3:GetObject",
+    ]
+    resources = [
+      "${aws_s3_bucket.frontend-cert-bucket.arn}/*"
+    ]
+    principals = [
+      "${aws_instance.radius.*.arn}"
+    ]
+  }
+}
+
+resource "aws_s3_bucket_policy" "frontend-cert-bucket-policy" {
+  bucket = "${aws_s3_bucket.frontend-cert-bucket.id}"
+  policy = "${aws_iam_policy_document.frontend-cert-bucket-policy-document.json}"
+}
