4d8257e08b188a772f8df0cf59fc7f7711344ada
Emile Swarts <emile@madetech.com>
2019-01-24 15:53:57 +0000

    Bump MYSQL engine version (#183)
    Amazon updates this automatically, change terraform to reflect this
    update

--- govwifi-backend/db.tf
+++ govwifi-backend/db.tf
@@ -95,7 +95,7 @@ resource "aws_db_instance" "db" {
   allocated_storage           = "${var.db-storage-gb}"
   storage_type                = "gp2"
   engine                      = "mysql"
-  engine_version              = "5.7.16"
+  engine_version              = "5.7.23"
   auto_minor_version_upgrade  = true
   allow_major_version_upgrade = false
   apply_immediately           = true
