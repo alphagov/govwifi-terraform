1e124e7e881525bb73107aed3e4743cd8782218c
Emile Swarts <emile@madetech.com>
2019-01-28 10:34:16 +0000

    Bump Mysql engine version to 5.7.23 (#185)
    AWS has upgraded this for us, and this just catches up the Terraform

--- govwifi-admin/db.tf
+++ govwifi-admin/db.tf
@@ -48,7 +48,7 @@ resource "aws_db_instance" "admin_db" {
   allocated_storage           = "${var.db-storage-gb}"
   storage_type                = "gp2"
   engine                      = "mysql"
-  engine_version              = "5.7.16"
+  engine_version              = "5.7.23"
   auto_minor_version_upgrade  = true
   allow_major_version_upgrade = false
   apply_immediately           = true
