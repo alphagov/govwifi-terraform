6af82570c891da41aae81de8db0870125c2b76c3
Emile <emile.swarts123@gmail.com>
2019-02-07 15:17:21 +0000

    Create CloudWatch alarms for the new users database

---
+++ govwifi-backend/user-db-alarms.tf
@@ -0,0 +1,179 @@
+resource "aws_cloudwatch_metric_alarm" "user_db_cpualarm" {
+  count               = "${var.db-instance-count}"
+  alarm_name          = "${var.env}-user--db-cpu-alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/RDS"
+  period              = "60"
+  statistic           = "Maximum"
+  threshold           = "80"
+
+  dimensions {
+    DBInstanceIdentifier = "${aws_db_instance.users_db.identifier}"
+  }
+
+  alarm_description  = "This metric monitors the cpu utilization of the User DB."
+  alarm_actions      = ["${var.critical-notifications-arn}"]
+  treat_missing_data = "breaching"
+}
+
+resource "aws_cloudwatch_metric_alarm" "user_db_memoryalarm" {
+  count               = "${var.db-instance-count}"
+  alarm_name          = "${var.env}-user-db-memory-alarm"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "FreeableMemory"
+  namespace           = "AWS/RDS"
+  period              = "60"
+  statistic           = "Minimum"
+  threshold           = "524288000"
+
+  dimensions {
+    DBInstanceIdentifier = "${aws_db_instance.users_db.identifier}"
+  }
+
+  alarm_description  = "This metric monitors the freeable memory available for the DB."
+  alarm_actions      = ["${var.critical-notifications-arn}"]
+  treat_missing_data = "breaching"
+}
+
+resource "aws_cloudwatch_metric_alarm" "user_db_storagealarm" {
+  count               = "${var.db-instance-count}"
+  alarm_name          = "${var.env}-user-db-storage-alarm"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "FreeStorageSpace"
+  namespace           = "AWS/RDS"
+  period              = "60"
+  statistic           = "Minimum"
+  threshold           = "21474836480"
+
+  dimensions {
+    DBInstanceIdentifier = "${aws_db_instance.users_db.identifier}"
+  }
+
+  alarm_description  = "This metric monitors the storage space available for the DB."
+  alarm_actions      = ["${var.capacity-notifications-arn}"]
+  treat_missing_data = "breaching"
+}
+
+resource "aws_cloudwatch_metric_alarm" "user_db_burstbalancealarm" {
+  count               = "${var.db-instance-count}"
+  alarm_name          = "${var.env}-user-db-burstbalanace-alarm"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "BurstBalance"
+  namespace           = "AWS/RDS"
+  period              = "180"
+  statistic           = "Minimum"
+  threshold           = "45"
+
+  dimensions {
+    DBInstanceIdentifier = "${aws_db_instance.users_db.identifier}"
+  }
+
+  alarm_description  = "This metric monitors the IOPS burst balance available for the DB."
+  alarm_actions      = ["${var.critical-notifications-arn}"]
+  treat_missing_data = "missing"
+}
+
+resource "aws_cloudwatch_metric_alarm" "user_rr_burstbalancealarm" {
+  count               = "${var.db-replica-count}"
+  alarm_name          = "${var.env}-user-rr-burstbalanace-alarm"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "BurstBalance"
+  namespace           = "AWS/RDS"
+  period              = "180"
+  statistic           = "Minimum"
+  threshold           = "45"
+
+  dimensions {
+    DBInstanceIdentifier = "${aws_db_instance.users_read_replica.identifier}"
+  }
+
+  alarm_description  = "This metric monitors the IOPS burst balance available for the DB read replica."
+  alarm_actions      = ["${var.capacity-notifications-arn}"]
+  treat_missing_data = "missing"
+}
+
+resource "aws_cloudwatch_metric_alarm" "user_rr_laggingalarm" {
+  count               = "${var.db-replica-count}"
+  alarm_name          = "${var.env}-user-rr-lagging-alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "ReplicaLag"
+  namespace           = "AWS/RDS"
+  period              = "60"
+  statistic           = "Minimum"
+  threshold           = "600"
+
+  dimensions {
+    DBInstanceIdentifier = "${aws_db_instance.users_read_replica.identifier}"
+  }
+
+  alarm_description  = "This metric monitors the Replication Lag for the DB read replica."
+  alarm_actions      = ["${var.capacity-notifications-arn}"]
+  treat_missing_data = "breaching"
+}
+
+resource "aws_cloudwatch_metric_alarm" "user_rr_cpualarm" {
+  count               = "${var.db-replica-count}"
+  alarm_name          = "${var.env}-user-rr-cpu-alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/RDS"
+  period              = "60"
+  statistic           = "Maximum"
+  threshold           = "80"
+
+  dimensions {
+    DBInstanceIdentifier = "${aws_db_instance.users_read_replica.identifier}"
+  }
+
+  alarm_description  = "This metric monitors the cpu utilization of the DB read replica."
+  alarm_actions      = ["${var.capacity-notifications-arn}"]
+  treat_missing_data = "breaching"
+}
+
+resource "aws_cloudwatch_metric_alarm" "user_rr_memoryalarm" {
+  count               = "${var.db-replica-count}"
+  alarm_name          = "${var.env}-user-rr-memory-alarm"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "FreeableMemory"
+  namespace           = "AWS/RDS"
+  period              = "60"
+  statistic           = "Minimum"
+  threshold           = "524288000"
+
+  dimensions {
+    DBInstanceIdentifier = "${aws_db_instance.users_read_replica.identifier}"
+  }
+
+  alarm_description  = "This metric monitors the freeable memory available for the DB read replica."
+  alarm_actions      = ["${var.capacity-notifications-arn}"]
+  treat_missing_data = "breaching"
+}
+
+resource "aws_cloudwatch_metric_alarm" "user_rr_storagealarm" {
+  count               = "${var.db-replica-count}"
+  alarm_name          = "${var.env}-user-rr-storage-alarm"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "FreeStorageSpace"
+  namespace           = "AWS/RDS"
+  period              = "60"
+  statistic           = "Minimum"
+  threshold           = "21474836480"
+
+  dimensions {
+    DBInstanceIdentifier = "${aws_db_instance.users_read_replica.identifier}"
+  }
+
+  alarm_description  = "This metric monitors the storage space available for the DB read replica."
+  alarm_actions      = ["${var.capacity-notifications-arn}"]
+  treat_missing_data = "breaching"
+}
