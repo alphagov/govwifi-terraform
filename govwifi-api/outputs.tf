a7f39cc40462cd769b9018d0f6f46f2be24d1c1b
Anthony King <anthonydking@gmail.com>
2019-03-01 10:48:26 +0000

    add a global dns name for the api cluster (#223)
    * add a global dns name for the api cluster
    
    * add certificate generation
    
    * add the new url as an output
    
    * fetch the port from the loaad balancer
    
    * remove redundant comment

---
+++ govwifi-api/outputs.tf
@@ -0,0 +1,4 @@
+output "api-base-url" {
+  description = "Global base url for API endpoints"
+  value       = "https://${aws_route53_record.elb_global.0.fqdn}:${aws_alb_listener.alb_listener.port}"
+}
