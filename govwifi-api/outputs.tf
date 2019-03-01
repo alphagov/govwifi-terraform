a13227a39647541a4212287721097af6e9ffa375
Anthony King <anthony@madetech.com>
2019-03-01 10:44:43 +0000

    fetch the port from the loaad balancer

--- govwifi-api/outputs.tf
+++ govwifi-api/outputs.tf
@@ -2,5 +2,5 @@ output "api-base-url" {
   description = "Global base url for API endpoints"
 
   # port 8443 is defined by by the ECS
-  value = "https://${aws_route53_record.elb_global.0.fqdn}:8443"
+  value = "https://${aws_route53_record.elb_global.0.fqdn}:${aws_alb_listener.alb_listener.port}"
 }
