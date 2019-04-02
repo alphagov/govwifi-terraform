85babda2ab6a9f2f2cca3f382a6cc8ebd9895e39
Anthony King <anthonydking@gmail.com>
2019-04-02 15:41:57 +0100

    Add metrics + alarms for the frontend service (#235)
    * add metric + alarm for count of shared secret invalid on auth
    
    I've deliberately set aside accounting being incorrect as we don't utilise accounting.
    
    * add outer identities alarm, and unknown client metric

--- govwifi-frontend/alarms.tf
+++ govwifi-frontend/alarms.tf
@@ -40,3 +40,35 @@ resource "aws_cloudwatch_metric_alarm" "radius-latency" {
     "${var.route53-critical-notifications-arn}",
   ]
 }
+
+resource "aws_cloudwatch_metric_alarm" "auth-shared-secret-incorrect" {
+  alarm_name          = "${var.Env-Name}-shared-secret-incorrect"
+  comparison_operator = "GreaterThanThreshold"
+  threshold           = 0
+  evaluation_periods  = 1
+  period              = "${60 * 60 * 24}"
+  statistic           = "Sum"
+  treat_missing_data  = "breaching"
+  metric_name         = "${aws_cloudwatch_log_metric_filter.auth-shared-secret-incorrect.metric_transformation.0.name}"
+  namespace           = "${aws_cloudwatch_log_metric_filter.auth-shared-secret-incorrect.metric_transformation.0.namespace}"
+
+  alarm_actions = [
+    "${var.devops-notifications-arn}",
+  ]
+}
+
+resource "aws_cloudwatch_metric_alarm" "outer-and-inner-identities-same" {
+  alarm_name          = "${var.Env-Name}-outer-and-inner-identities-same"
+  comparison_operator = "GreaterThanThreshold"
+  threshold           = 0
+  evaluation_periods  = 1
+  period              = "${60 * 60 * 24}"
+  statistic           = "Sum"
+  treat_missing_data  = "breaching"
+  metric_name         = "${aws_cloudwatch_log_metric_filter.outer-and-inner-identities-same.metric_transformation.0.name}"
+  namespace           = "${aws_cloudwatch_log_metric_filter.outer-and-inner-identities-same.metric_transformation.0.namespace}"
+
+  alarm_actions = [
+    "${var.devops-notifications-arn}",
+  ]
+}
---
+++ govwifi-frontend/metrics.tf
@@ -0,0 +1,55 @@
+resource "aws_cloudwatch_log_metric_filter" "auth-shared-secret-incorrect" {
+  name = "${var.Env-Name}-auth-shared-secret-incorrect"
+
+  pattern        = "\"Shared secret is incorrect\" \"Received packet\""
+  log_group_name = "${aws_cloudwatch_log_group.frontend-log-group.name}"
+
+  metric_transformation {
+    name          = "auth-shared-secret-incorrect-count"
+    namespace     = "${local.frontend_metrics_namespace}"
+    value         = "1"
+    default_value = "0"
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "accounting-shared-secret-incorrect" {
+  name = "${var.Env-Name}-accounting-shared-secret-incorrect"
+
+  pattern        = "\"Shared secret is incorrect\" \"Received Accounting-Request packet\""
+  log_group_name = "${aws_cloudwatch_log_group.frontend-log-group.name}"
+
+  metric_transformation {
+    name          = "accounting-shared-secret-incorrect-count"
+    namespace     = "${local.frontend_metrics_namespace}"
+    value         = "1"
+    default_value = "0"
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "outer-and-inner-identities-same" {
+  name = "${var.Env-Name}-outer-and-inner-identities-same"
+
+  pattern        = "\"Outer and inner identities are the same\""
+  log_group_name = "${aws_cloudwatch_log_group.frontend-log-group.name}"
+
+  metric_transformation {
+    name          = "outer-and-inner-identities-same-count"
+    namespace     = "${local.frontend_metrics_namespace}"
+    value         = "1"
+    default_value = "0"
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "unknown-client" {
+  name = "${var.Env-Name}-unknown-client"
+
+  pattern        = "\"unknown client\""
+  log_group_name = "${aws_cloudwatch_log_group.frontend-log-group.name}"
+
+  metric_transformation {
+    name          = "unknown-client"
+    namespace     = "${local.frontend_metrics_namespace}"
+    value         = "1"
+    default_value = "0"
+  }
+}
