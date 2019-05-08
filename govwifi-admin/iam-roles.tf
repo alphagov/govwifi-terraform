efbe3397de47aed47f9922d072442d066eb5830d
Tom Taylor <tommotaylor@gmail.com>
2019-05-08 14:21:42 +0100

    Add IAM role for writing to product page data bucket (#242)

--- govwifi-admin/iam-roles.tf
+++ govwifi-admin/iam-roles.tf
@@ -75,6 +75,15 @@ resource "aws_iam_role_policy" "ecs-admin-instance-policy" {
         "s3:ListBucket"
       ],
       "Resource": ["${aws_s3_bucket.admin-mou-bucket.arn}"]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:PutObject",
+        "s3:PutObjectAcl",
+        "s3:PutObjectVersionAcl"
+      ],
+      "Resource": ["${aws_s3_bucket.product-page-data-bucket.arn}/*"]
     }
   ]
 }
