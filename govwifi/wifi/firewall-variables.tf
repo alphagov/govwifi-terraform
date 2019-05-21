6821fadff04d397cad0eb39ac22c5aace6b66169
Anthony King <anthony@madetech.com>
2019-05-21 19:25:05 +0100

    add production ireland terraform config
    with redactions

---
+++ govwifi/wifi/firewall-variables.tf
@@ -0,0 +1,16 @@
+# Also used for the new site setup PDF - unless overridden in main.tf
+variable "frontend-radius-IPs" {
+}
+
+# The frontend RADIUS IPs for the current region - used for EIP association
+variable "frontend-region-IPs" {
+}
+
+variable "administrator-IPs" {
+}
+
+variable "bastion-server-IP" {
+}
+
+variable "backend-subnet-IPs" {
+}
