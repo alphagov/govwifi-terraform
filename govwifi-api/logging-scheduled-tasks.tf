f1efebe2782fc5b9e155364779ce38e1bd821ee0
Anthony King <anthony@madetech.com>
2019-05-23 12:30:26 +0100

    add new task to bulk update user last logins

--- govwifi-api/logging-scheduled-tasks.tf
+++ govwifi-api/logging-scheduled-tasks.tf
@@ -125,6 +125,32 @@ resource "aws_cloudwatch_event_target" "logging-daily-session-deletion" {
 EOF
 }
 
+
+resource "aws_cloudwatch_event_target" "gdpr-set-user-last-login" {
+  count     = "${var.logging-enabled}"
+  target_id = "${var.Env-Name}-gdpr-user-set-last-login"
+  arn       = "${aws_ecs_cluster.api-cluster.arn}"
+  rule      = "${aws_cloudwatch_event_rule.daily_gdpr_set_user_last_login.name}"
+  role_arn  = "${aws_iam_role.logging-scheduled-task-role.arn}"
+
+  ecs_target = {
+    task_count          = 1
+    task_definition_arn = "${aws_ecs_task_definition.logging-api-scheduled-task.arn}"
+    launch_type         = "EC2"
+  }
+
+  input = <<EOF
+{
+  "containerOverrides": [
+    {
+      "name": "logging",
+      "command": ["bundle", "exec", "rake", "update_yesterdays_last_login"]
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_ecs_task_definition" "logging-api-scheduled-task" {
   count         = "${var.logging-enabled}"
   family        = "logging-api-scheduled-task-${var.Env-Name}"
