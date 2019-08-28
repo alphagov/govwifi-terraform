ccba9f424c2e7006ad45af103b4b35a121d89b6a
Rafal Proszowski <rafal.proszowski@digital.cabinet-office.gov.uk>
2019-08-28 23:03:46 +0100

    Add two new custom metrics
    We'd like to be monitoring SLIs properly. CloudWatch makes it difficult
    for us to mix and match the metrics ang logs. We're creating a custom
    metric based on logs and are assuming CloudWatch will take care of that
    for us.

--- govwifi-api/locals.tf
+++ govwifi-api/locals.tf
@@ -1,3 +1,4 @@
 locals {
-  logging_api_namespace = "${var.Env-Name}-logging-api"
+  logging_api_namespace       = "${var.Env-Name}-logging-api"
+  authorisation_api_namespace = "${var.Env-Name}-authorisation-api"
 }
