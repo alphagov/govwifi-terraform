ab9c97b433287c228f5f45e9cd31f9eec0d235c4
Sarah C Y <sarahseewhy@gmail>
2019-11-01 16:48:48 +0000

    feat: add radius server and accounting listeners
    * Follows up from @rjbaker's PR comments.
    * Resource names gleaned from `fe-radius-in` configuration in `security_group.tf`.
    
    Co-authored by: Sarah Young <sarah.young@digital.cabinet-office.gov.uk>

--- govwifi-frontend/nlb.tf
+++ govwifi-frontend/nlb.tf
@@ -12,33 +12,60 @@ resource "aws_lb" "frontend-nlb" {
 }
 
 resource "aws_lb_target_group" "frontend-target-group" {
-  name     = "frontend-tg-${var.Env-Name}"
+  name     = "front-tg-staging"
   port     = 3000
-  protocol = "UDP"
+  protocol = "TCP_UDP"
   vpc_id   = "${aws_vpc.wifi-frontend.id}"
 
   health_check {
     healthy_threshold   = 3
     unhealthy_threshold = 3
     interval            = 30
-    protocol            = "TCP"
+    protocol            = "HTTP"
+    path                = "/"
   }
 
   tags = {
     Name = "frontend-tg-${var.Env-Name}"
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-resource "aws_lb_target_group_attachment" "frontend-target-group-attachment" {
+resource "aws_lb_target_group_attachment" "frontend-healthcheck" {
   count            = "${aws_instance.radius.count}"
   target_group_arn = "${aws_lb_target_group.frontend-target-group.arn}"
   target_id        = "${element(aws_instance.radius.*.id, count.index)}"
-  port             = 8080
+  port             = 3000
 }
 
-resource "aws_lb_listener" "frontend-listener" {
+resource "aws_lb_listener" "healthcheck" {
   load_balancer_arn = "${aws_lb.frontend-nlb.arn}"
   port              = 3000
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.frontend-target-group.arn}"
+  }
+}
+
+resource "aws_lb_listener" "radius-server" {
+  load_balancer_arn = "${aws_lb.frontend-nlb.arn}"
+  port              = 1812
+  protocol          = "UDP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.frontend-target-group.arn}"
+  }
+}
+
+resource "aws_lb_listener" "radius-accounting" {
+  load_balancer_arn = "${aws_lb.frontend-nlb.arn}"
+  port              = 1813
   protocol          = "UDP"
 
   default_action {
