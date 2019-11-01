936a0e3d4c346641adf98f5c68cf38dd80effd52
Sarah C Y <sarahseewhy@gmail>
2019-11-01 15:00:51 +0000

    refactor: align naming and tags with project standard
    * The project target groups are tagged using the format `<service>-tg-<env>`.
    * The other load balancers and target groups follow a naming convention for `name` : `<service>-<env>-<load balancer type>`.
    
    Co-authored by: Sarah Young <sarah.young@digital.cabinet-office.gov.uk>

--- govwifi-frontend/nlb.tf
+++ govwifi-frontend/nlb.tf
@@ -1,12 +1,10 @@
 resource "aws_lb" "frontend-nlb" {
-  name               = "frontend-nlb"
+  name               = "frontend-${var.Env-Name}-nlb"
   internal           = false
   load_balancer_type = "network"
   subnets            = ["${aws_subnet.wifi-frontend-subnet.*.id}"]
 
   enable_deletion_protection = false
-
-  ip_address_type = "ipv4"
 
   tags = {
     Name = "frontend-nlb-${var.Env-Name}"
@@ -14,7 +12,7 @@ resource "aws_lb" "frontend-nlb" {
 }
 
 resource "aws_lb_target_group" "frontend-target-group" {
-  name     = "frontend-target-group"
+  name     = "frontend-${var.Env-Name}-tg"
   port     = 3000
   protocol = "UDP"
   vpc_id   = "${aws_vpc.wifi-frontend.id}"
@@ -24,6 +22,10 @@ resource "aws_lb_target_group" "frontend-target-group" {
     unhealthy_threshold = 3
     interval            = 30
     protocol            = "TCP"
+  }
+
+  tags = {
+    Name = "frontend-tg-${var.Env-Name}"
   }
 }
 
