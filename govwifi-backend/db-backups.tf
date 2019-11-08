3438bdbb2a2eacc4435f9f6a172e4882891bf7c3
Sarah C Y <sarahseewhy@gmail>
2019-11-08 16:13:57 +0000

    feat: create database backup S3 bucket and policy
    * Creates S3 bucket and attaches policy denying any delete actions.
    * This is a first step in disabling previous backup process.
    
    Co-authored by: Sarah Young <sarah.young@digital.cabinet-office.gov.uk>

---
+++ govwifi-backend/db-backups.tf
@@ -0,0 +1,43 @@
+resource "aws_s3_bucket" "db_backups" {
+  bucket        = "${var.Env-Name}-${lower(var.aws-region-name)}-db-backup"
+  force_destroy = true
+  acl           = "private"
+
+  tags = {
+    Name   = "${title(var.Env-Name)} Database backup"
+    Region = "${title(var.aws-region-name)}"
+    Environment = "${title(var.Env-Name)}"
+  }
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_s3_bucket_policy" "db_backups" {
+  bucket = "${aws_s3_bucket.db_backups.id}"
+  policy = "${data.aws_iam_policy_document.db_backups.json}"
+}
+
+data "aws_iam_policy_document" "db_backups" {
+  statement {
+    effect = "Deny"
+
+    principals {
+      identifiers = ["*"]
+      type = "AWS"
+    }
+
+    actions = [
+      "s3:DeleteBucket",
+      "s3:DeleteObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.db_backups.arn}",
+      "${aws_s3_bucket.db_backups.arn}/*"
+    ]
+  }
+}
+
+
