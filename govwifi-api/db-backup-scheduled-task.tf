42859ced6b16dcd36aff7b26b237cd7b1fd6e075
Sarah C Y <sarahseewhy@gmail>
2019-11-11 14:11:26 +0000

    feat: add cloudwatch event rule
    * The cloudwatch event rule will trigger the scheduled task to add the database backup from the ECR repo to the S3 bucket at 1AM every day.
    * The log group keeps logs of the scheduled task.
    
    Co-authored by: Sarah Young <sarah.young@digital.cabinet-office.gov.uk>

--- govwifi-api/db-backup-scheduled-task.tf
+++ govwifi-api/db-backup-scheduled-task.tf
@@ -1,3 +1,16 @@
 resource "aws_ecr_repository" "database_backups" {
   name = "govwifi/database-backups"
 }
+
+resource "aws_cloudwatch_event_rule" "daily_databse_backup" {
+  name                = "${var.Env-Name}-daily-database-backup"
+  description         = "Triggers at 1am Daily"
+  schedule_expression = "cron(0 1 * * ? *)"
+  is_enabled          = true
+}
+
+resource "aws_cloudwatch_log_group" "database_back_up_log_group" {
+  name = "${var.Env-Name}-databse-backup-log-group"
+
+  retention_in_days = 90
+}
