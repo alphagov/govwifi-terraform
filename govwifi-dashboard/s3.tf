9e7f060afcb134615e09df2383299b7fbb673133
Stéphane Maniaci <stephane.maniaci@gmail.com>
2020-06-04 15:04:54 +0100

    fix: remove the metrics bucket roles
    It was my (mis)understanding to create roles (read-only/write-only)
    around this bucket that would then be distributed to other parts of
    the terraformation but:
    
    1. it's not really possible;
    2. it's a lot easier to just add the according bucket's name in the
    relevant modules's role policies.

--- govwifi-dashboard/s3.tf
+++ govwifi-dashboard/s3.tf
@@ -1,10 +1,11 @@
 resource "aws_s3_bucket" "metrics-bucket" {
-  bucket        = "metrics-bucket"
+  bucket        = "govwifi-${var.Env-Name}-metrics-bucket"
   force_destroy = true
   acl           = "private"
 
   tags = {
     Name        = "${title(var.Env-Name)} Metrics data"
+    Environment = "${title(var.Env-Name)}"
   }
 
   versioning {
@@ -12,52 +13,7 @@ resource "aws_s3_bucket" "metrics-bucket" {
   }
 }
 
-resource "aws_iam_role" "metrics-read-only-role" {
-  name = "dashboard-metrics-read-only-role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy" "metrics-read-only-policy" {
-  name = "metrics-bucket-read-only-policy"
-  role = aws_iam_role.metrics-read-only-role.id
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": ["s3:ListBucket"],
-      "Resource": ["${aws_s3_bucket.metrics-bucket.arn}"]
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:ListObjectsV2",
-        "s3:GetObject",
-      ],
-      "Resource": ["${aws_s3_bucket.metrics-bucket.arn}/*"]
-    }
-  ]
-}
-EOF
-}
-
-output "instance_ip_addr" {
+output "metrics_bucket_arn" {
+  description = "the ARN for the metrics bucket"
   value = aws_s3_bucket.metrics-bucket.arn
 }
