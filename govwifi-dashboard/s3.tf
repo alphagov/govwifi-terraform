810f979bcd408f76b3d8a2a38fe5d256e4cc4b95
Stéphane Maniaci <stephane.maniaci@gmail.com>
2020-06-08 13:58:37 +0100

    dashboard: move output to a separate file

--- govwifi-dashboard/s3.tf
+++ govwifi-dashboard/s3.tf
@@ -12,8 +12,3 @@ resource "aws_s3_bucket" "metrics-bucket" {
     enabled = true
   }
 }
-
-output "metrics_bucket_arn" {
-  description = "the ARN for the metrics bucket"
-  value = aws_s3_bucket.metrics-bucket.arn
-}
