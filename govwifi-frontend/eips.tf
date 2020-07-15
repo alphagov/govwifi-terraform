830c47fb63cf657219e6edd1d86faff0a63dee21
Stephen Ford <stephen.ford@digital.cabinet-office.gov.uk>
2020-07-15 14:23:21 +0100

    Make Radius EIPS get provisioned via terraform
    Currently the EIPs used for the radius servers are not managed by
    terraform, this makes them get managed by terraform.
    
    Mob: @sarahseewhy, @camdesgov & @smford

---
+++ govwifi-frontend/eips.tf
@@ -0,0 +1,14 @@
+resource "aws_eip" "radius-eips" {
+  count = "${var.radius-instance-count}"
+  vpc   = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags {
+    Name   = "${title(var.Env-Name)} Frontend Radius-${var.dns-numbering-base + count.index + 1}"
+    Region = "${title(var.aws-region-name)}"
+    Env    = "${title(var.Env-Name)}"
+  }
+}
