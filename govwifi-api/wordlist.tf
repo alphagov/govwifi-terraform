9cdf4d9e99102c5f80445216cda393097f510c70
Stephen Ford <stephen.ford@digital.cabinet-office.gov.uk>
2020-07-29 18:25:33 +0100

    Create a unique wordlist bucket for staging-temp
    There is a shared wordlist bucket used between production and old
    staging.  This is not ideal and is tech dept that needs to be fixed
    before this branch is merged into master.
    
    This commit hard bakes a new bucket name in to support the provisioning
    of the staging-temp environment.

--- govwifi-api/wordlist.tf
+++ govwifi-api/wordlist.tf
@@ -1,10 +1,10 @@
 resource "aws_s3_bucket" "wordlist" {
-  bucket = "govwifi-wordlist"
+  bucket = "govwifi-staging-temp-wordlist"
   count  = var.wordlist-bucket-count
   acl    = "private"
 
   tags = {
-    Name = "wordlist-bucket"
+    Name = "wordlist-staging-temp-bucket"
   }
 
   versioning {
@@ -46,4 +46,3 @@ resource "aws_iam_user" "jenkins-read-wordlist-bucket" {
   name  = "jenkins-read-wordlist-user"
   count = var.wordlist-bucket-count
 }
-
