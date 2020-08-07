0960ed889b3f4f658a3cc8809039369ee19afa65
Stéphane Maniaci <stephane.maniaci@gmail.com>
2020-08-07 16:24:15 +0100

    feat: add and schedule tasks to publish active users metrics
    The new active users metrics are almost identical to the metrics sent
    to the PerformancePlatform (i.e `publish_daily_statistics`) except
    they're called `publish_daily_metrics`.
    
    Re-use most of the previous code but use `metrics` in identifier and
    schedule:
    
    * the daily metrics generation at 2AM;
    * the weekly metrics generation on Sundays at 5:45AM;
    * the monthly metrics generation every 1st of the month at 6AM.

--- govwifi-api/cloudwatch-events.tf
+++ govwifi-api/cloudwatch-events.tf
@@ -69,3 +69,28 @@ resource "aws_cloudwatch_event_rule" "daily_gdpr_set_user_last_login" {
   schedule_expression = "cron(0 2 * * ? *)"
   is_enabled          = true
 }
+
+# new daily, weekly and monthly active users metrics published to S3
+resource "aws_cloudwatch_event_rule" "daily_active_users_metrics_event" {
+  count               = "${var.event-rule-count}"
+  name                = "${var.Env-Name}-daily-active-users-metrics-logging"
+  description         = "Triggers daily 02:00 am UTC"
+  schedule_expression = "cron(0 2 * * ? *)"
+  is_enabled          = true
+}
+
+resource "aws_cloudwatch_event_rule" "weekly_active_users_metrics_event" {
+  count               = "${var.event-rule-count}"
+  name                = "${var.Env-Name}-weekly-active-users-metrics-logging"
+  description         = "Triggers every SUN 05:45 am UTC"
+  schedule_expression = "cron(45 5 ? * 1 *)"
+  is_enabled          = true
+}
+
+resource "aws_cloudwatch_event_rule" "monthly_active_users_metrics_event" {
+  count               = "${var.event-rule-count}"
+  name                = "${var.Env-Name}-monthly-active-users-metrics-logging"
+  description         = "Triggers on the first of each month at 06:00 am UTC"
+  schedule_expression = "cron(0 6 1 * ? *)"
+  is_enabled          = true
+}
