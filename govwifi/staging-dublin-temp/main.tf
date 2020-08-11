298becd86ceb57f620bb725d86ef4cda3d3b931d
Sarah Young <sarah.young@digital.cabinet-office.gov.uk>
2020-08-11 09:45:18 +0100

    WIP: re-instate the module but leave it commented out instead of completely removed.

--- govwifi/staging-dublin-temp/main.tf
+++ govwifi/staging-dublin-temp/main.tf
@@ -124,7 +124,26 @@ module "backend" {
 }
 
 # Emails ======================================================================
-
+//module "emails" {
+//  providers = {
+//    "aws" = "aws.AWS-main"
+//  }
+//
+//  source                   = "../../govwifi-emails"
+//  product-name             = "${var.product-name}"
+//  Env-Name                 = "${var.Env-Name}"
+//  Env-Subdomain            = "${var.Env-Subdomain}"
+//  aws-account-id           = "${var.aws-account-id}"
+//  route53-zone-id          = "${var.route53-zone-id}"
+//  aws-region               = "${var.aws-region}"
+//  aws-region-name          = "${var.aws-region-name}"
+//  mail-exchange-server     = "10 inbound-smtp.eu-west-1.amazonaws.com"
+//  devops-notifications-arn = "${module.notifications.topic-arn}"
+//
+//  #sns-endpoint             = "https://elb.${lower(var.aws-region-name)}.${var.Env-Subdomain}.service.gov.uk/sns/"
+//  sns-endpoint                       = "https://elb.london.${var.Env-Subdomain}.service.gov.uk/sns/"
+//  user-signup-notifications-endpoint = "https://user-signup-api.${var.Env-Subdomain}.service.gov.uk:8443/user-signup/email-notification"
+//}
 
 module "govwifi-keys" {
   providers = {
