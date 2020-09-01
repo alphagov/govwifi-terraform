a02020355312b7d58c04237fdb18c83a2a00b9c7
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2020-09-01 15:47:40 +0100

    Move the retrieve notifications event from logging to signup-api
    Because the rake task is defined in the govwifi-signup-api repo

--- govwifi-api/logging-scheduled-tasks.tf
+++ govwifi-api/logging-scheduled-tasks.tf
@@ -57,43 +57,6 @@ resource "aws_iam_role_policy" "logging-scheduled-task-policy" {
 DOC
 }
 
-resource "aws_cloudwatch_event_target" "retrieve-notifications" {
-  count     = "${var.logging-enabled}"
-  target_id = "${var.Env-Name}-retrieve-notifications"
-  arn       = "${aws_ecs_cluster.api-cluster.arn}"
-  rule      = "${aws_cloudwatch_event_rule.retrieve_notifications_event.name}"
-  role_arn  = "${aws_iam_role.logging-scheduled-task-role.arn}"
-
-  ecs_target = {
-    task_count          = 1
-    task_definition_arn = "${aws_ecs_task_definition.logging-api-scheduled-task.arn}"
-    launch_type         = "FARGATE"
-
-    network_configuration = {
-      subnets = ["${var.subnet-ids}"]
-
-      security_groups = [
-        "${var.backend-sg-list}",
-        "${aws_security_group.api-in.id}",
-        "${aws_security_group.api-out.id}",
-      ]
-
-      assign_public_ip = true
-    }
-  }
-
-  input = <<EOF
-{
-  "containerOverrides": [
-    {
-      "name": "logging",
-      "command": ["bundle", "exec", "rake", "retrieve_notifications"]
-    }
-  ]
-}
-EOF
-}
-
 resource "aws_cloudwatch_event_target" "logging-publish-weekly-statistics" {
   count     = "${var.logging-enabled}"
   target_id = "${var.Env-Name}-logging-weekly-statistics"
