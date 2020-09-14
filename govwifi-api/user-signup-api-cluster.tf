c075b7f15a2307529ffe5712bda1980ced952088
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2020-09-14 15:06:07 +0100

    Add S3_METRICS_BUCKET environment variable to user_signup_api task

--- govwifi-api/user-signup-api-cluster.tf
+++ govwifi-api/user-signup-api-cluster.tf
@@ -152,6 +152,10 @@ resource "aws_ecs_task_definition" "user-signup-api-task" {
         },{
           "name": "GOVNOTIFY_BEARER_TOKEN",
           "value": "${var.govnotify-bearer-token}"
+        },
+        {
+          "name": "S3_METRICS_BUCKET",
+          "value": "${var.metrics-bucket-name}"
         }
       ],
       "links": null,
