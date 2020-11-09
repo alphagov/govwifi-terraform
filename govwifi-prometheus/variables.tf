420c3ed0e500b66e2511e6c5c7096c68c86d19ca
Camille Descartes <camille.descartes@digital.cabinet-office.gov.uk>
2020-11-09 17:14:48 +0000

    Prometheus Log Exporter Spike

---
+++ govwifi-prometheus/variables.tf
@@ -0,0 +1,92 @@
+variable "Env-Name" {}
+
+variable "Env-Subdomain" {}
+
+variable "route53-zone-id" {}
+
+variable "vpc-cidr-block" {}
+
+variable "aws-region" {}
+
+variable "aws-region-name" {}
+
+### Added for Prometheus
+
+variable "ami" {
+  description = "AMI id to launch, must be in the region specified by the region variable"
+}
+
+### Editing begins again HERE!!!
+
+
+
+
+
+
+
+
+variable "radius-instance-sg-ids" {
+  type = "list"
+}
+
+variable "frontend-docker-image" {}
+
+variable "raddb-docker-image" {}
+
+
+
+variable "ssh-key-name" {}
+
+variable "shared-key" {}
+
+variable "healthcheck-radius-key" {}
+variable "healthcheck-ssid" {}
+variable "healthcheck-identity" {}
+variable "healthcheck-password" {}
+
+variable "dns-numbering-base" {}
+
+variable "logging-api-base-url" {}
+
+variable "auth-api-base-url" {}
+
+variable "elastic-ip-list" {
+  type = "list"
+}
+
+variable "enable-detailed-monitoring" {}
+
+variable "radiusd-params" {
+  default = "-f"
+}
+
+variable "users" {
+  type = "list"
+}
+
+variable "rack-env" {
+  default = ""
+}
+
+variable "create-ecr" {
+  description = "Whether or not to create ECR repository"
+  default     = false
+}
+
+variable "bastion-ips" {
+  description = "The list of allowed hosts to connect to the ec2 instances"
+  type        = "list"
+  default     = []
+}
+
+variable "route53-critical-notifications-arn" {
+  type = "string"
+}
+
+variable "devops-notifications-arn" {
+  type = "string"
+}
+
+variable "admin-bucket-name" {
+  type = "string"
+}
