91b7d7959774a0e2e38f0fcfad46927b7f41047c
Colin Burn-Murdoch <colin.burn-murdoch@digital.cabinet-office.gov.uk>
2021-01-27 09:48:30 +0000

    Adds a new cron job to delete smoke test users

--- govwifi-api/cloudwatch-events.tf
+++ govwifi-api/cloudwatch-events.tf
@@ -62,6 +62,14 @@ resource "aws_cloudwatch_event_rule" "daily_user_deletion_event" {
   is_enabled          = true
 }
 
+resource "aws_cloudwatch_event_rule" "smoke_test_user_deletion_event" {
+  count               = "${var.event-rule-count}"
+  name                = "${var.Env-Name}-smoke-test-user-deletion"
+  description         = "Triggers daily 1:00 am UTC"
+  schedule_expression = "cron(30 23 * * ? *)"
+  is_enabled          = true
+}
+
 resource "aws_cloudwatch_event_rule" "daily_gdpr_set_user_last_login" {
   count               = "${var.event-rule-count}"
   name                = "${var.Env-Name}-daily-gdpr-set-user-last-login"
--- govwifi-api/user-signup-scheduled-tasks.tf
+++ govwifi-api/user-signup-scheduled-tasks.tf
@@ -345,6 +345,42 @@ resource "aws_cloudwatch_event_target" "user-signup-daily-user-deletion" {
 }
 EOF
 }
+resource "aws_cloudwatch_event_target" "smoke-test-user-deletion" {
+  count     = "${var.user-signup-enabled}"
+  target_id = "${var.Env-Name}-smoke-test-user-deletion"
+  arn       = "${aws_ecs_cluster.api-cluster.arn}"
+  rule      = "${aws_cloudwatch_event_rule.smoke_test_user_deletion_event.name}"
+  role_arn  = "${aws_iam_role.user-signup-scheduled-task-role.arn}"
+
+  ecs_target = {
+    task_count          = 1
+    task_definition_arn = "${aws_ecs_task_definition.user-signup-api-scheduled-task.arn}"
+    launch_type         = "FARGATE"
+
+    network_configuration = {
+      subnets = ["${var.subnet-ids}"]
+
+      security_groups = [
+        "${var.backend-sg-list}",
+        "${aws_security_group.api-in.id}",
+        "${aws_security_group.api-out.id}",
+      ]
+
+      assign_public_ip = true
+    }
+  }
+
+  input = <<EOF
+{
+  "containerOverrides": [
+    {
+      "name": "user-signup",
+      "command": ["bundle", "exec", "rake", "delete_smoke_test_users"]
+    }
+  ]
+}
+EOF
+}
 
 resource "aws_ecs_task_definition" "user-signup-api-scheduled-task" {
   count                    = "${var.user-signup-enabled}"
