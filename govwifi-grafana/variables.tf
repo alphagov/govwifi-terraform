8393db651d89ab3cdfe4f4e2a7960177432db817
Sarah Young <sarah.young@digital.cabinet-office.gov.uk>
2021-02-01 14:31:43 +0000

    Added descriptions to the variables.

--- govwifi-grafana/variables.tf
+++ govwifi-grafana/variables.tf
@@ -1,15 +1,29 @@
-variable "Env-Name" {}
-
-variable "aws-region" {}
-
-variable "ssh-key-name" {}
-
-variable "backend-subnet-ids" {
-  type = "list"
+variable "Env-Name" {
+  description = "Environment name for the component. Value is inherited from the main deployment file."
+  type        = "string"
 }
 
-variable "be-admin-in" {}
+variable "aws-region" {
+  description = "AWS region for the component. Value is inherited from the main deployment file."
+  type        = "string"
+}
+
+variable "ssh-key-name" {
+  description = "The SSH key name to use. Value is inherited from the main deployment file."
+  type        = "string"
+}
+
+variable "backend-subnet-ids" {
+  description = "The subnets associated with the backend VPC. Values are inherited from the main deployment file."
+  type        = "list"
+}
+
+variable "be-admin-in" {
+  description = "The relevant security group for this component. Value is inherited from the main deployment file."
+  type        = "string"
+}
 
 variable "create_grafana_server" {
-  default = "0"
+  description = "A feature toggle for the Grafana instance (1 = enabled; value defaults to disabled and is overwritten in the main deployment file.)"
+  default     = "0"
 }
