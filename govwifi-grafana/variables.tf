9602ebfa7ec69c6cad1c40d411d755d8d8c2bd96
Sarah Young <sarah.young@digital.cabinet-office.gov.uk>
2021-02-01 14:46:25 +0000

    Add variables required for security groups and the new application load balancer.
    Also added comment explaining how values are derived which allowed me to remove redundant descriptions.

--- govwifi-grafana/variables.tf
+++ govwifi-grafana/variables.tf
@@ -1,29 +1,48 @@
+# Unless there is a default provided, all values are inherited from the main deployment file.
+# Values can also be overwritten from the main deployment file.
+
 variable "Env-Name" {
-  description = "Environment name for the component. Value is inherited from the main deployment file."
+  description = "Environment name for the component."
   type        = "string"
 }
 
 variable "aws-region" {
-  description = "AWS region for the component. Value is inherited from the main deployment file."
+  description = "AWS region for the component."
   type        = "string"
 }
 
 variable "ssh-key-name" {
-  description = "The SSH key name to use. Value is inherited from the main deployment file."
+  description = "The SSH key name to use."
   type        = "string"
 }
 
 variable "backend-subnet-ids" {
-  description = "The subnets associated with the backend VPC. Values are inherited from the main deployment file."
+  description = "The subnets associated with the backend VPC."
   type        = "list"
 }
 
 variable "be-admin-in" {
-  description = "The relevant security group for this component. Value is inherited from the main deployment file."
+  description = "The relevant security group for this component."
   type        = "string"
 }
 
 variable "create_grafana_server" {
   description = "A feature toggle for the Grafana instance (1 = enabled; value defaults to disabled and is overwritten in the main deployment file.)"
   default     = "0"
+}
+
+variable "vpc-id" {
+  description = "VPC ID used for the Application Load Balancer."
+  type        = "string"
+}
+
+variable "bastion-ips" {
+  description = "The list of allowed hosts to connect to the EC2 instances."
+  type        = "list"
+  default     = []
+}
+
+variable "subnet-ids" {
+  description = "List of AWS subnet IDs to place the EC2 instances and ELB into."
+  type        = "list"
 }
