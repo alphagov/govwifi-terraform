144a7e19ed3877eb388cadc7dbe946459abb8d66
Sarah Young <sarah.young@digital.cabinet-office.gov.uk>
2021-02-01 16:04:02 +0000

    Add initial Grafana Application Load Balancer
    * Based on the configuration we used for the Admin API.
    * Creates an ALB with relevant security groups.
    * Creates an ALB listener, listening on port 443.
    * Finally, creates a target group for a health check (this may not be necessary).
    * I've left out the required certificate ARN and SSL policy on the ALB for now (this required Route53 and ACM configuration which I'd like to pair on first).

---
+++ govwifi-grafana/loadbalancer.tf
@@ -0,0 +1,49 @@
+resource "aws_lb" "grafana-alb" {
+  name     = "grafana-alb-${var.Env-Name}"
+  internal = false
+  subnets  = ["${var.subnet-ids}"]
+
+  security_groups = [
+    "${aws_security_group.grafana-alb-in.id}",
+    "${aws_security_group.grafana-alb-out.id}",
+  ]
+
+  load_balancer_type = "application"
+
+  tags = {
+    Name = "grafana-alb-${var.Env-Name}"
+  }
+}
+
+resource "aws_alb_listener" "alb_listener" {
+  load_balancer_arn = "${aws_lb.grafana-alb.arn}"
+  port              = "443"
+  protocol          = "HTTPS"
+
+  default_action {
+    target_group_arn = "${aws_alb_target_group.grafana-tg.arn}"
+    type             = "forward"
+  }
+}
+
+resource "aws_alb_target_group" "grafana-tg" {
+  depends_on           = ["aws_lb.grafana-alb"]
+  name                 = "grafana-${var.Env-Name}-fg-tg"
+  port                 = "3000"
+  protocol             = "HTTP"
+  vpc_id               = "${var.vpc-id}"
+  target_type          = "ip"
+  deregistration_delay = 10
+
+  health_check {
+    healthy_threshold   = 3
+    unhealthy_threshold = 10
+    timeout             = 5
+    interval            = 10
+    path                = "/healthcheck"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
