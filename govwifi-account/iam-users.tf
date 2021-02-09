eb4bca704ede7d9f69c4e7f0685f4ce67f710ece
Ian Nicholls <ian.nicholls@digital.cabinet-office.gov.uk>
2021-02-09 14:30:53 +0000

    changed spaces from 4 to 2

--- govwifi-account/iam-users.tf
+++ govwifi-account/iam-users.tf
@@ -1,60 +1,60 @@
 # Current users
 
 resource "aws_iam_user" "monitor" {
-    name = "monitor"
-    path = "/"
+name = "monitor"
+  path = "/"
 }
 
 ### Users to be changed
 
 # Old user to rename and split into 3 - will retire once no longer in use and below 3 are
 resource "aws_iam_user" "govwifi-jenkins-deploy" {
-    name = "govwifi-jenkins-deploy"
-    path = "/"
+  name = "govwifi-jenkins-deploy"
+  path = "/"
 }
 # New User from above for prod
 resource "aws_iam_user" "govwifi-pipeline-deploy-prod" {
-    name = "govwifi-pipeline-deploy-prod"
-    path = "/"
+  name = "govwifi-pipeline-deploy-prod"
+  path = "/"
 }
 # New User from above for admin 
 resource "aws_iam_user" "govwifi-pipeline-deploy-admin" {
-    name = "govwifi-pipeline-deploy-admin"
-    path = "/"
+  name = "govwifi-pipeline-deploy-admin"
+  path = "/"
 }
 # New User from above for staging
 resource "aws_iam_user" "govwifi-pipeline-deploy-staging" {
-    name = "govwifi-pipeline-deploy-staging"
-    path = "/"
+  name = "govwifi-pipeline-deploy-staging"
+  path = "/"
 }
 
 # Old username - rename from jenkins to pipeline to make agnostic
 resource "aws_iam_user" "govwifi-jenkins-terraform" {
-    name = "govwifi-jenkins-terraform"
-    path = "/"
+  name = "govwifi-jenkins-terraform"
+  path = "/"
 }
 # New replacement
 resource "aws_iam_user" "govwifi-pipeline-terraform" {
-    name = "govwifi-pipeline-terraform"
-    path = "/"
+  name = "govwifi-pipeline-terraform"
+  path = "/"
 }
 
 ### Users no longer needed?
 
-# Not used currently?
+# Not used currently? Deactivated and awaiting screaming before re-activating
 resource "aws_iam_user" "jenkins-read-wordlist-user" {
-    name = "jenkins-read-wordlist-user"
-    path = "/"
+  name = "jenkins-read-wordlist-user"
+  path = "/"
 }
 resource "aws_iam_user" "servicedesk" {
-    name = "servicedesk"
-    path = "/"
+  name = "servicedesk"
+  path = "/"
 }
 resource "aws_iam_user" "dashboard-staging-read-only-user" {
-    name = "dashboard-staging-read-only-user"
-    path = "/"
+  name = "dashboard-staging-read-only-user"
+  path = "/"
 }
 resource "aws_iam_user" "dashboard-wifi-read-only-user" {
-    name = "dashboard-wifi-read-only-user"
-    path = "/"
+  name = "dashboard-wifi-read-only-user"
+  path = "/"
 }
