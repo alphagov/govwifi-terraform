6060b143c990cfdaaa579765a3f05a37a4a47b35
Ian Nicholls <ian.nicholls@digital.cabinet-office.gov.uk>
2021-02-12 15:44:22 +0000

    changed tabs to spaces

--- govwifi-account/iam-users.tf
+++ govwifi-account/iam-users.tf
@@ -11,43 +11,43 @@ resource "aws_iam_user" "monitor" {
 
 # Old user to rename and split into 3 - will retire once no longer in use and below 3 are
 resource "aws_iam_user" "govwifi-jenkins-deploy" {
-  name		= "govwifi-jenkins-deploy"
-  path		= "/"
+  name          = "govwifi-jenkins-deploy"
+  path          = "/"
   force_destroy = false
 }
 
 # New User from above for prod
 resource "aws_iam_user" "govwifi-pipeline-deploy-prod" {
-  name		= "govwifi-pipeline-deploy-prod"
-  path		= "/"
+  name          = "govwifi-pipeline-deploy-prod"
+  path          = "/"
   force_destroy = false
 }
 
 # New User from above for admin 
 resource "aws_iam_user" "govwifi-pipeline-deploy-admin" {
-  name		= "govwifi-pipeline-deploy-admin"
-  path		= "/"
+  name          = "govwifi-pipeline-deploy-admin"
+  path          = "/"
   force_destroy = false
 }
 
 # New User from above for staging
 resource "aws_iam_user" "govwifi-pipeline-deploy-staging" {
-  name		= "govwifi-pipeline-deploy-staging"
-  path		= "/"
+  name          = "govwifi-pipeline-deploy-staging"
+  path          = "/"
   force_destroy = false
 }
 
 # Old username - rename from jenkins to pipeline to make agnostic
 resource "aws_iam_user" "govwifi-jenkins-terraform" {
-  name		= "govwifi-jenkins-terraform"
-  path		= "/"
+  name          = "govwifi-jenkins-terraform"
+  path          = "/"
   force_destroy = false
 }
 
 # New replacement
 resource "aws_iam_user" "govwifi-pipeline-terraform" {
-  name		= "govwifi-pipeline-terraform"
-  path		= "/"
+  name          = "govwifi-pipeline-terraform"
+  path          = "/"
   force_destroy = false
 }
 
@@ -55,26 +55,26 @@ resource "aws_iam_user" "govwifi-pipeline-terraform" {
 
 # Not used currently? Deactivated and awaiting screaming before re-activating
 resource "aws_iam_user" "jenkins-read-wordlist-user" {
-  name		= "jenkins-read-wordlist-user"
-  path		= "/"
+  name          = "jenkins-read-wordlist-user"
+  path          = "/"
   force_destroy = false
 }
 
 resource "aws_iam_user" "servicedesk" {
-  name		= "servicedesk"
-  path		= "/"
+  name          = "servicedesk"
+  path          = "/"
   force_destroy = false
 }
 
 resource "aws_iam_user" "dashboard-staging-read-only-user" {
-  name		= "dashboard-staging-read-only-user"
-  path		= "/"
+  name          = "dashboard-staging-read-only-user"
+  path          = "/"
   force_destroy = false
 }
 
 resource "aws_iam_user" "dashboard-wifi-read-only-user" {
-  name		= "dashboard-wifi-read-only-user"
-  path		= "/"
+  name          = "dashboard-wifi-read-only-user"
+  path          = "/"
   force_destroy = false
 }
 
