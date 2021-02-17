3fd3ba56cab6749ffe0ee7a39214779f09289abf
cchani <charles.chani@digital.cabinet-office.gov.uk>
2021-02-17 16:23:35 +0000

    staging grafana

--- govwifi-grafana/route53.tf
+++ govwifi-grafana/route53.tf
@@ -2,6 +2,7 @@ resource "aws_route53_record" "grafana-staging" {
   zone_id = "${data.aws_route53_zone.zone.id}"
   name    = "grafana.${var.Env-Subdomain}.service.gov.uk"
   type    = "CNAME"
+  ttl     = "300"
   records = ["d_700724072c65db3cc82b0c4c13b529e6.hkmpvcwbzw.acm-validations.aws."]
 }
 
