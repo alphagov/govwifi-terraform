c54ec4dd6462d0b97ad987c762f54bcc909b98c2
Camille Descartes <camille.descartes@digital.cabinet-office.gov.uk>
2021-02-17 17:06:17 +0000

    Change DNS Record To A From CNAME

--- govwifi-grafana/route53.tf
+++ govwifi-grafana/route53.tf
@@ -1,10 +1,16 @@
 resource "aws_route53_record" "grafana-staging" {
   zone_id = "${data.aws_route53_zone.zone.id}"
   name    = "grafana.${var.Env-Subdomain}.service.gov.uk"
-  type    = "CNAME"
-  ttl     = "300"
-  records = ["d_700724072c65db3cc82b0c4c13b529e6.hkmpvcwbzw.acm-validations.aws."]
+  type    = "A"
+
+  alias {
+    name                   = "${aws_lb.grafana-staging-alb.dns_name}"
+    zone_id                = "${aws_lb.grafana-staging-alb.zone_id}"
+    evaluate_target_health = true
+  }
+
 }
+
 
 data "aws_route53_zone" "zone" {
   name         = "wifi.service.gov.uk."
