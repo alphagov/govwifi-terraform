54329b5ea11640081f7c1abe92455d5075f02ffb
cchani <charles.chani@digital.cabinet-office.gov.uk>
2021-04-13 16:10:59 +0100

    Update the Bastian hosts AMI's for both eu-west-1 and eu-west-2

--- govwifi/staging-london/main.tf
+++ govwifi/staging-london/main.tf
@@ -85,7 +85,9 @@ module "backend" {
   frontend-radius-IPs = var.frontend-radius-IPs
 
   # eu-west-2, CIS Ubuntu Linux 16.04 LTS Benchmark v1.0.0.4 - Level 1
-  bastion-ami                = "ami-ae6d81c9"
+  #  bastion-ami                = "ami-ae6d81c9"
+  # eu-west-2 eu-west-2, CIS Ubuntu Linux 20.04 LTS 
+  bastion-ami                = "ami-096cb92bb3580c759"
   bastion-instance-type      = "t2.micro"
   bastion-server-ip          = var.bastion-server-IP
   bastion-ssh-key-name       = "govwifi-staging-bastion-key-20181025"
