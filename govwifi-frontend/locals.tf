38bcdbfaff9fa8f915b3a452db33b46c7014796e
Sarah Young <sarah.young@digital.cabinet-office.gov.uk>
2021-04-16 14:41:44 +0100

    Retrieve healthcheck ssid from Secrets Manager as a `local` resource.
    Replace use of variable with local value in cluster declaration.

--- govwifi-frontend/locals.tf
+++ govwifi-frontend/locals.tf
@@ -5,3 +5,7 @@ locals {
 locals {
   healthcheck_identity = jsondecode(data.aws_secretsmanager_secret_version.healthcheck_identity.secret_string)["identity"]
 }
+
+locals {
+  healthcheck_ssid = jsondecode(data.aws_secretsmanager_secret_version.healthcheck_ssid.secret_string)["ssid"]
+}
