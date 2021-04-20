dbd76ba7921df254b0ddd2864ba3d4ec59e9d763
Sarah Young <sarah.young@digital.cabinet-office.gov.uk>
2021-04-20 14:11:02 +0100

    Remove unused local values

--- govwifi-frontend/locals.tf
+++ govwifi-frontend/locals.tf
@@ -1,11 +1,3 @@
 locals {
   frontend_metrics_namespace = "${var.Env-Name}-frontend"
 }
-
-locals {
-  healthcheck_identity = jsondecode(data.aws_secretsmanager_secret_version.healthcheck_identity.secret_string)["identity"]
-}
-
-locals {
-  healthcheck_ssid = jsondecode(data.aws_secretsmanager_secret_version.healthcheck_ssid.secret_string)["ssid"]
-}
