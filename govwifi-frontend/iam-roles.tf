8a9ade6fdaed1ad4ac85f93c20f80d3644cb339d
Sarah Young <sarah.young@digital.cabinet-office.gov.uk>
2021-04-23 11:45:52 +0100

    Set healthcheck SSID from Secrets Manager not as an ENV variable

--- govwifi-frontend/iam-roles.tf
+++ govwifi-frontend/iam-roles.tf
@@ -201,7 +201,8 @@ data "aws_iam_policy_document" "secrets_manager_policy" {
     ]
 
     resources = [
-      data.aws_secretsmanager_secret.healthcheck_identity.arn
+      data.aws_secretsmanager_secret.healthcheck_identity.arn,
+      data.aws_secretsmanager_secret.healthcheck_ssid.arn
     ]
   }
 }
