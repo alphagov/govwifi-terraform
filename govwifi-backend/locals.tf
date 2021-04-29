58023d427570b4ae9deba7da49c1aeb33dfe9d92
Sarah Young <sarah.young@digital.cabinet-office.gov.uk>
2021-04-29 15:57:54 +0100

    refactor: remove user-db credential retrieval
    * Descoping this work for now. Focusing first on the RADIUS healthcheck credentials as a first step.
    * I'm not ready to add the production user-db credentials to Secrets Manager yet.

--- govwifi-backend/locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  users_db_username = jsondecode(data.aws_secretsmanager_secret_version.users_db_credentials.secret_string)["username"]
-}
-
-locals {
-  users_db_password = jsondecode(data.aws_secretsmanager_secret_version.users_db_credentials.secret_string)["password"]
-}
