46d541324bcb9c597454a04e1fceb48e30175c69
Sarah Young <sarah.young@digital.cabinet-office.gov.uk>
2021-04-29 12:29:00 +0100

    refactor: format variables file

--- govwifi/staging-london/variables.tf
+++ govwifi/staging-london/variables.tf
@@ -313,7 +313,7 @@ variable "volumetrics-elasticsearch-endpoint" {
 }
 
 variable "use_env_prefix" {
-  default = true
-  type = bool
+  default     = true
+  type        = bool
   description = "Conditional to indicate whether to retrieve a secret with a env prefix in its name."
 }
