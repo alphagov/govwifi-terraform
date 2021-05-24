68374bc07ffc97b94ac72821db0aa228f4c93a72
Sarah Young <sarah.young@digital.cabinet-office.gov.uk>
2021-05-24 15:11:20 +0100

    refactor: create resource-specific files for iam and vpc endpoint code
    * These resource aren't used by the `intances.tf` and don't directly relate to the configuration or provisioning of the instance.
    * Make it easier to build on these resources.

--- govwifi-grafana/instances.tf
+++ govwifi-grafana/instances.tf
@@ -100,23 +100,3 @@ resource "aws_vpc_endpoint" "vpc-endpoint" {
 
   timeouts {}
 }
-
-data "aws_iam_policy_document" "secrets_manager_policy" {
-  statement {
-    actions = [
-      "secretsmanager:GetSecretValue"
-    ]
-
-    resources = [
-      data.aws_secretsmanager_secret.google_client_id.arn,
-      data.aws_secretsmanager_secret.google_client_secret.arn,
-      data.aws_secretsmanager_secret.grafana_admin.arn,
-      data.aws_secretsmanager_secret.grafana_server_root_url.arn
-    ]
-
-    principals {
-      identifiers = ["ec2.amazonaws.com"]
-      type = "Service"
-    }
-  }
-}
