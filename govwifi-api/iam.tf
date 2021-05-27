98edaebe62f705bfcf58ead333c9e2950c8cd8a9
Sarah Young <sarah.young@digital.cabinet-office.gov.uk>
2021-05-27 14:39:54 +0100

    refactor: append GovWifi service name to ECS task definition role
    * This aligns the naming across the clusters (frontend, api, and admin).
    * Removing the SecretsManager suffix caused a "Role with name ecsTaskExecutionRole-staging-London already exists" error.

--- govwifi-api/iam.tf
+++ govwifi-api/iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "ecsTaskExecutionRole" {
-  name               = "ecsTaskExecutionRole-${var.rack-env}-${var.aws-region-name}"
+  name               = "api-ecsTaskExecutionRole-${var.rack-env}-${var.aws-region-name}"
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 }
 
