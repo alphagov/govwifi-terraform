d0817b3d09d92e7710cb73186507fe90ad187e03
Sarah Young <sarah.young@digital.cabinet-office.gov.uk>
2021-05-27 11:40:40 +0100

    refactor: abstract frontend ecsTaskExecutionRole name
    * Remove unnecessary "SecretsManager" suffix.
    * The frontend ECS cluster only has this one ecsTaskExecutionRole and although it's only used to retrieve secrets from Secrets Manager the other ECS clusters don't follow this naming pattern.

--- govwifi-frontend/iam-roles.tf
+++ govwifi-frontend/iam-roles.tf
@@ -166,7 +166,7 @@ data "aws_iam_policy_document" "admin_bucket_policy" {
 }
 
 resource "aws_iam_role" "ecsTaskExecutionRole" {
-  name               = "ecsTaskExecutionRole-${var.rack-env}-${var.aws-region-name}-SecretsManager"
+  name               = "ecsTaskExecutionRole-${var.rack-env}-${var.aws-region-name}"
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 }
 
