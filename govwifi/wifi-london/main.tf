cfbb4f4f4856e2669e4006fba54cebbd89a2d6fd
Sarah Young <sarah.young@digital.cabinet-office.gov.uk>
2021-06-09 11:53:52 +0100

    feat: remove unused variable
    * Slack workplace ID is now retrieved from Secrets Manager, not `govwifi-build`

--- govwifi/wifi-london/main.tf
+++ govwifi/wifi-london/main.tf
@@ -530,7 +530,6 @@ module "govwifi-slack-alerts" {
   critical-notifications-topic-arn         = module.critical-notifications.topic-arn
   capacity-notifications-topic-arn         = module.capacity-notifications.topic-arn
   route53-critical-notifications-topic-arn = module.route53-critical-notifications.topic-arn
-  gds-slack-workplace-id                   = var.gds-slack-workplace-id
   gds-slack-channel-id                     = var.gds-slack-channel-id
 }
 
