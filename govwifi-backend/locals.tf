c13f0bfa17b81aaa4e1bff0094cd139e6fbde059
Sarah Young <sarah.young@digital.cabinet-office.gov.uk>
2021-06-10 11:27:45 +0100

    feat: retrieve User-Details DB credentials from Secrets Manager
    * Create the Terraform to retrieve DB username and password from Secrets manager.
    * The values have been manually created in Secrets Manager already.
    * Running `plan` returns "No changes. Infrastructure is up-to-date." since the values are unchanged.

--- govwifi-backend/locals.tf
+++ govwifi-backend/locals.tf
@@ -5,3 +5,11 @@ locals {
 locals {
   session_db_password = jsondecode(data.aws_secretsmanager_secret_version.session_db_credentials.secret_string)["password"]
 }
+
+locals {
+  users_db_username = jsondecode(data.aws_secretsmanager_secret_version.users_db_credentials.secret_string)["username"]
+}
+
+locals {
+  users_db_password = jsondecode(data.aws_secretsmanager_secret_version.users_db_credentials.secret_string)["password"]
+}
