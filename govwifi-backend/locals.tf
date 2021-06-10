2d5f1dfabc2b710da1323e93a6fa8f68e3e17db4
Sarah Young <sarah.young@digital.cabinet-office.gov.uk>
2021-06-10 11:24:58 +0100

    refactor: Session DB resource naming

--- govwifi-backend/locals.tf
+++ govwifi-backend/locals.tf
@@ -1,7 +1,7 @@
 locals {
-  session_db_username = jsondecode(data.aws_secretsmanager_secret_version.db_session_credentials.secret_string)["username"]
+  session_db_username = jsondecode(data.aws_secretsmanager_secret_version.session_db_credentials.secret_string)["username"]
 }
 
 locals {
-  session_db_password = jsondecode(data.aws_secretsmanager_secret_version.db_session_credentials.secret_string)["password"]
+  session_db_password = jsondecode(data.aws_secretsmanager_secret_version.session_db_credentials.secret_string)["password"]
 }
