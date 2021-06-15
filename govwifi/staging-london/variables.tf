db84b45e0c811cdfd39eca115c0ffec16775ed46
Sarah Young <sarah.young@digital.cabinet-office.gov.uk>
2021-06-15 15:06:45 +0100

    feat: remove unused device-wifi-docker-image-path variable
    * This value is not used in the codebase

--- govwifi/staging-london/variables.tf
+++ govwifi/staging-london/variables.tf
@@ -148,11 +148,6 @@ variable "london-api-base-url" {
   default     = "https://api-elb.london.staging.wifi.service.gov.uk:8443"
 }
 
-variable "device-wifi-docker-image-path" {
-  type        = string
-  description = "ARN used to identify the common path element used for the docker image repositories."
-}
-
 variable "govnotify-bearer-token" {
   type = string
 }
