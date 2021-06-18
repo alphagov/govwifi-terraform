a0dd65224536bd4269ea87b1ad48faf4374a22f5
David Pye <david.pye@digital.cabinet-office.gov.uk>
2021-06-18 16:35:18 +0100

    remove legacy lines left over from source file

--- govwifi-prometheus/iam-roles.tf
+++ govwifi-prometheus/iam-roles.tf
@@ -1,13 +1,9 @@
 resource "aws_iam_instance_profile" "prometheus-instance-profile" {
-  // count      = var.enable-bastion
   name       = "${var.aws-region}-${var.Env-Name}-prometheus-instance-profile"
   role       = aws_iam_role.prometheus-instance-role.name
-  // depends_on = [aws_iam_role.prometheus-instance-role]
 }
 
-
 resource "aws_iam_role" "prometheus-instance-role" {
-  // count = var.enable-prometheus
   name  = "${var.aws-region}-${var.Env-Name}-prometheus-instance-role"
   path = "/"
 
@@ -30,10 +26,8 @@ EOF
 }
 
 resource "aws_iam_role_policy" "prometheus-instance-policy" {
-  // count      = min(1 - var.save-pp-data, var.enable-bastion)  ???
   name       = "${var.aws-region}-${var.Env-Name}-prometheus-instance-policy"
   role       = aws_iam_role.prometheus-instance-role.id
-  // depends_on = [aws_iam_role.prometheus-instance-role]
 
   policy = <<EOF
 {
