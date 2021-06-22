55f24b1bdb6d75320808e09c0a2fd1d23001629a
Ian Nicholls <ian.nicholls@digital.cabinet-office.gov.uk>
2021-06-22 13:42:37 +0100

    added an alert when someone adds/updates a secret

---
+++ govwifi-backend/secrets-manager-alarms.tf
@@ -0,0 +1,19 @@
+#
+resource "aws_cloudwatch_event_rule" "secretmanager_putsecretvalue_rule" {
+  count       = 1
+  name        = "${var.env}-PutSecretValue-Alarm"
+  description = "This metric monitors any Secrets in SecretManager that have been written (new or update)"
+
+  event_pattern = <<EOF
+{
+  "EventName": [ "PutSecretValue" ]
+}
+EOF
+
+}
+
+resource "aws_cloudwatch_event_target" "secretmanager_putsecretvalue_sns" {
+  rule      = aws_cloudwatch_event_rule.secretmanager_putsecretvalue_rule[0].name
+  target_id = "SendToSNS"
+  arn       = aws_sns_topic.govwifi-wifi-critical.arn
+}
