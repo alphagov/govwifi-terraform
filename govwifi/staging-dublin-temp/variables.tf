94c474f0e8cfc02c2e7526ca2862ca38182dfb38
Camille Descartes <camille.descartes@digital.cabinet-office.gov.uk>
2021-08-19 16:21:14 +0100

    Add Conditional Secrets Prefix Flag To Secondary Staging

--- govwifi/staging-dublin-temp/variables.tf
+++ govwifi/staging-dublin-temp/variables.tf
@@ -165,3 +165,9 @@ variable "administrator-IPs-list" {
 variable "backend-subnet-IPs-list" {
   description = "Unused in this configuration"
 }
+
+variable "use_env_prefix" {
+  default     = false
+  type        = bool
+  description = "Conditional to indicate whether to retrieve a secret with a env prefix in its name. For the secondary account the value can be set to false. The 'staging' prefix is redundant since the secondary account will be used for staging"
+}
