314e379f917e1db82541da5883c0256d542a61b4
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2021-09-27 11:20:49 +0100

    Use underscores for resource names in the govwifi-dashboard module
    Try to move towards using underscores rather than dashes, as this is
    more consistent with Terraform itself. Just change the resources
    within the govwifi-dashboard module.
    
    This shouldn't have any effects beyond the names used in Terraform
    changing. Unfortunately, when applying this change, either the
    existing resources will need to be moved (using the state mv command
    for example), or need destroying and re-creating through running
    terraform apply.

--- govwifi-dashboard/outputs.tf
+++ govwifi-dashboard/outputs.tf
@@ -1,5 +1,5 @@
 output "metrics-bucket-name" {
   description = "the name for the metrics bucket"
-  value       = aws_s3_bucket.metrics-bucket.id
+  value       = aws_s3_bucket.metrics_bucket.id
 }
 
