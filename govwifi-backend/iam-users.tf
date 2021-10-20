005775c38072a7d96bf557920d07a640ef45932a
Camille Descartes <camille.descartes@digital.cabinet-office.gov.uk>
2021-10-20 18:14:37 +0100

    Update policy formatting
    Old policy code kept throwing json errors whenever a terraform plan was run.
    Not sure if this is because we updated terraform. Adding extra indentation to
    these lines made the error disappear.

--- govwifi-backend/iam-users.tf
+++ govwifi-backend/iam-users.tf
@@ -16,32 +16,32 @@ resource "aws_iam_user_policy" "backup_s3_read_buckets_user_policy" {
 
   policy = <<POLICY
 {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "sid0",
-      "Effect": "Allow",
-      "Action": [
-        "s3:ListBucket"
-      ],
-      "Resource": [
-        "arn:aws:s3:::${local.rds_mysql_backup_bucket}"
-      ]
-    },
-    {
-      "Sid": "sid1",
-      "Effect": "Allow",
-      "Action": [
-        "s3:GetObject",
-        "s3:GetObjectVersion",
-        "kms:Decrypt"
-      ],
-      "Resource": [
-        "arn:aws:kms:eu-west-2:${var.aws_account_id}:key/*",
-        "arn:aws:s3:::${local.rds_mysql_backup_bucket}/*",
-      ]
-    }
-  ]
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "sid0",
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListBucket"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${local.rds_mysql_backup_bucket}"
+            ]
+        },
+        {
+            "Sid": "sid1",
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:GetObjectVersion",
+                "kms:Decrypt"
+            ],
+            "Resource": [
+                "arn:aws:kms:eu-west-2:${var.aws_account_id}:key/*",
+                "arn:aws:s3:::${local.rds_mysql_backup_bucket}/*"
+            ]
+        }
+    ]
 }
 POLICY
 
