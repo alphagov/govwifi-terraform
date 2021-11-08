fcf488efd4295a8f9af7a6dd28241f7403824045
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2021-11-08 09:51:03 +0000

    Rename the api-base-url output in the govwifi-api module
    The direction is to converge on using underscores rather than dashes
    in output names, this is more consistent with Terraform itself.
    
    This output appears to be unused, at least in the Terraform.

--- govwifi-api/outputs.tf
+++ govwifi-api/outputs.tf
@@ -1,4 +1,4 @@
-output "api-base-url" {
+output "api_base_url" {
   description = "Global base url for API endpoints"
   value       = "https://${aws_route53_record.elb_global[0].fqdn}:${aws_alb_listener.alb_listener.port}"
 }
