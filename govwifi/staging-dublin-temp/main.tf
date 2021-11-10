fbde495999e2dc75b6dbcb1dad8c907a8942230a
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2021-11-10 16:59:47 +0000

    Adjust staging for the addition of us_east_1_pagerduty_notifications_arn
    This should have happened back in
    b5e38d1d9b97f2afa990034487028a8d74a97410, but was missed.

--- govwifi/staging-dublin-temp/main.tf
+++ govwifi/staging-dublin-temp/main.tf
@@ -227,8 +227,9 @@ module "frontend" {
   logging_api_base_url = var.london_api_base_url
   auth_api_base_url    = var.dublin_api_base_url
 
-  critical_notifications_arn           = module.notifications.topic_arn
-  us_east_1_critical_notifications_arn = module.route53-notifications.topic_arn
+  critical_notifications_arn            = module.notifications.topic_arn
+  us_east_1_critical_notifications_arn  = module.route53-notifications.topic_arn
+  us_east_1_pagerduty_notifications_arn = data.terraform_remote_state.london.outputs.us_east_1_notifications_topic_arn
 
   bastion_server_ip = var.bastion_server_ip
 
--- govwifi/staging-london-temp/outputs.tf
+++ govwifi/staging-london-temp/outputs.tf
@@ -1,3 +1,7 @@
 output "admin_app_data_s3_bucket_name" {
   value = module.govwifi_admin.app_data_s3_bucket_name
 }
+
+output "us_east_1_notifications_topic_arn" {
+  value = module.route53-notifications.topic_arn
+}
