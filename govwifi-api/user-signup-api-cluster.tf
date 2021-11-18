59b4157bbd9af82da079597079f24f7d19cefb2c
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2021-11-18 14:30:11 +0000

    Tighten up the health checks for the API service ALBs
    Having a ALB instance wait for 10 consecutive failures at intervals of
    10 seconds is far too long, that would mean that a target would still
    be used for up to 100 seconds after it started timing out.
    
    I think 2 consecutive failures is a much better value, as that should
    result in unhealthy targets being removed faster. Decrease the timeout
    as well, 4 seconds is probably still too high, but I just wanted to
    decrease it a bit.

--- govwifi-api/user-signup-api-cluster.tf
+++ govwifi-api/user-signup-api-cluster.tf
@@ -215,9 +215,9 @@ resource "aws_alb_target_group" "user_signup_api_tg" {
   }
 
   health_check {
-    healthy_threshold   = 3
-    unhealthy_threshold = 10
-    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 4
     interval            = 10
     path                = "/healthcheck"
   }
