44ac20f673b33c86daece6e92c06d361a8225ec6
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2021-12-01 14:00:59 +0000

    Fix uses of var.env_subdomain which should be local.env_subdomain

--- govwifi/staging-dublin/data.tf
+++ govwifi/staging-dublin/data.tf
@@ -1,4 +1,4 @@
 # TODO This should probably be managed by Terraform at some point
 data "aws_route53_zone" "main" {
-  name = "${var.env_subdomain}.service.gov.uk."
+  name = "${local.env_subdomain}.service.gov.uk."
 }
