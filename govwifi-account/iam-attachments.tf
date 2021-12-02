17d908d852ab353bfae34b14a65364be747ca214
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2021-12-02 16:09:50 +0000

    Address IAM duplication between govwifi-account and govwifi-api
    Things like the read_wordlist_policy look to have been copied in to
    govwifi-api, but also left in govwifi-account, with the
    is_production_aws_account variable also added to avoid Terraform
    trying to create the same policy twice in production.
    
    Remove the use of is_production_aws_account by removing IAM things
    that look to be duplicated in govwifi-account. This is a little
    confusing since govwifi-account looks to grant access via a group,
    whereas govwifi-api manages it directly with users.

--- govwifi-account/iam-attachments.tf
+++ govwifi-account/iam-attachments.tf
@@ -8,23 +8,7 @@ resource "aws_iam_group_policy_attachment" "LambdaUpdateFunctionCode_policy_atta
   policy_arn = "arn:aws:iam::${var.aws_account_id}:policy/LambdaUpdateFunctionCode"
 }
 
-resource "aws_iam_group_policy_attachment" "AmazonEC2ContainerServiceEventsRole_policy_attachment_GovWifi_Pipeline" {
-  group      = "GovWifi-Pipeline"
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceEventsRole"
-}
-
-resource "aws_iam_group_policy_attachment" "AmazonEC2ContainerRegistryPowerUser_policy_attachment_GovWifi_Pipeline" {
-  group      = "GovWifi-Pipeline"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
-}
-
-resource "aws_iam_group_policy_attachment" "read_wordlist_policy_GovWifi_Pipeline" {
-  group      = "GovWifi-Pipeline"
-  policy_arn = "arn:aws:iam::${var.aws_account_id}:policy/read-wordlist-policy"
-}
-
 resource "aws_iam_group_policy_attachment" "can_restart_ecs_services_GovWifi_Pipeline" {
   group      = "GovWifi-Pipeline"
   policy_arn = "arn:aws:iam::${var.aws_account_id}:policy/can-restart-ecs-services"
 }
-
