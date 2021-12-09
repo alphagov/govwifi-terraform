3c680ddebe01c1619c67a3e52e2764a80daf3984
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2021-12-09 09:43:40 +0000

    Remove empty IAM groups from the production account
    All these IAM groups are empty (have no users), so they're not doing
    anything and can be removed.
    
    If there's some purpose for them to serve in the future, it probably
    needs re-thinking, as this Terraform doesn't apply to the staging
    account, just production.

--- govwifi-account/iam-attachments.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_iam_group_policy_attachment" "GovWifi_Audit_policy_attachment_GovWifi_Audit" {
-  group      = "GovWifi-Audit"
-  policy_arn = "arn:aws:iam::${var.aws_account_id}:policy/GovWifi-Audit"
-}
