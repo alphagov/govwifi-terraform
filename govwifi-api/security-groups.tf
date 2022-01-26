612f2525263f382552fedc731b8f172efa84c894
camdesgov <49409388+camdesgov@users.noreply.github.com>
2022-01-26 12:53:00 +0000

    Revert "Restrict traffic to User Signup API"

--- govwifi-api/security-groups.tf
+++ govwifi-api/security-groups.tf
@@ -32,21 +32,3 @@ resource "aws_security_group" "api_out" {
   }
 }
 
-resource "aws_security_group" "lambda_user_api_out" {
-  count       = var.user_signup_enabled
-  name        = "lambda-user-api-out"
-  description = "Allow Outbound Traffic From Lambda To User API via the internet"
-  vpc_id      = var.vpc_id
-
-  tags = {
-    Name = "${title(var.env_name)} User API Lambda Traffic Out"
-  }
-
-  egress {
-    to_port     = 8443
-    from_port   = 8443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow traffic out over internet so we can send messages to the user-sign-up api"
-  }
-}
--- govwifi-api/user-signup-api-security-groups.tf
+++ govwifi-api/user-signup-api-security-groups.tf
@@ -4,19 +4,10 @@ resource "aws_security_group" "user_signup_api_lb_in" {
   vpc_id = var.vpc_id
 
   ingress {
-    description = "Traffic from NAT gateway attached to user-api lambda"
     from_port   = 8443
     to_port     = 8443
     protocol    = "tcp"
-    cidr_blocks = [for ip in var.nat_gateway_elastic_ips : "${ip}/32"]
-  }
-
-  ingress {
-    description = "Traffic from Notify to confirm SNS signup"
-    from_port   = 8443
-    to_port     = 8443
-    protocol    = "tcp"
-    cidr_blocks = [for ip in var.notify_ips : "${ip}/32"]
-
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
