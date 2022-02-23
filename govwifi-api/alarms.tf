a88ac9984e38e4b2f5bebd1112cbee82c2689861
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2022-02-23 11:04:57 +0000

    Move user signup API alarms from new_alarms.tf-- to relevant module
    This is part of sorting out the new_alarms.tf-- mess.

--- govwifi-api/alarms.tf
+++ govwifi-api/alarms.tf
@@ -114,3 +114,45 @@ resource "aws_cloudwatch_metric_alarm" "user_signup_api_no_healthy_hosts" {
     var.notification_arn,
   ])
 }
+
+resource "aws_cloudwatch_metric_alarm" "user_signup_api_node_unhealthy" {
+  count = var.user_signup_enabled
+
+  alarm_name          = "GovWifi - Production - user-signup-api-wifi Node Unhealthy"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "UnHealthyHostCount"
+  namespace           = "AWS/ApplicationELB"
+  period              = "300"
+  statistic           = "Minimum"
+  threshold           = "0.0"
+  alarm_description   = "Failure of any ALB Node in \"user-signup-api-wifi\" consistently for a 5 minute period\n\nCheck the end points of the ALB to see if there is an issue\nCheck the end points to see if there is an issue"
+
+  alarm_actions = [var.capacity_notifications_arn]
+
+  dimensions = {
+    TargetGroup  = aws_alb_target_group.user_signup_api_tg[0].arn_suffix
+    LoadBalancer = aws_lb.user_signup_api[0].arn_suffix
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "user_signup_api_cpu_usage_high" {
+  count = var.user_signup_enabled
+
+  alarm_name          = "user signup CPU usage high (snowflake)"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "3"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "5.0"
+  alarm_description   = "user signup CPU high"
+
+  alarm_actions = [var.capacity_notifications_arn]
+
+  dimensions = {
+    ClusterName = aws_ecs_cluster.api_cluster.name
+    ServiceName = aws_ecs_service.user_signup_api_service[0].name
+  }
+}
