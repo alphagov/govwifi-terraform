21e48d7fd70f017dabfe99af4ebc646e7d8655e9
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2022-08-23 16:45:24 +0100

    Switch the frontend metrics to the Fargate cluster log group
    As these metrics should now come from the Fargate cluster logs.

--- govwifi-frontend/metrics.tf
+++ govwifi-frontend/metrics.tf
@@ -2,7 +2,7 @@ resource "aws_cloudwatch_log_metric_filter" "auth_shared_secret_incorrect" {
   name = "${var.env_name}-auth-shared-secret-incorrect"
 
   pattern        = "\"Shared secret is incorrect\" \"Received packet\""
-  log_group_name = aws_cloudwatch_log_group.frontend_log_group.name
+  log_group_name = aws_cloudwatch_log_group.frontend.name
 
   metric_transformation {
     name          = "auth-shared-secret-incorrect-count"
@@ -16,7 +16,7 @@ resource "aws_cloudwatch_log_metric_filter" "accounting_shared_secret_incorrect"
   name = "${var.env_name}-accounting-shared-secret-incorrect"
 
   pattern        = "\"Shared secret is incorrect\" \"Received Accounting-Request packet\""
-  log_group_name = aws_cloudwatch_log_group.frontend_log_group.name
+  log_group_name = aws_cloudwatch_log_group.frontend.name
 
   metric_transformation {
     name          = "accounting-shared-secret-incorrect-count"
@@ -30,7 +30,7 @@ resource "aws_cloudwatch_log_metric_filter" "outer_and_inner_identities_same" {
   name = "${var.env_name}-outer-and-inner-identities-same"
 
   pattern        = "\"Outer and inner identities are the same\""
-  log_group_name = aws_cloudwatch_log_group.frontend_log_group.name
+  log_group_name = aws_cloudwatch_log_group.frontend.name
 
   metric_transformation {
     name          = "outer-and-inner-identities-same-count"
@@ -44,7 +44,7 @@ resource "aws_cloudwatch_log_metric_filter" "unknown_client" {
   name = "${var.env_name}-unknown-client"
 
   pattern        = "\"unknown client\""
-  log_group_name = aws_cloudwatch_log_group.frontend_log_group.name
+  log_group_name = aws_cloudwatch_log_group.frontend.name
 
   metric_transformation {
     name          = "unknown-client"
@@ -58,7 +58,7 @@ resource "aws_cloudwatch_log_metric_filter" "radius_cannot_connect_to_api" {
   name = "${var.env_name}-radius-cannot-connect-to-api"
 
   pattern        = "\"ERROR: Server returned no data\""
-  log_group_name = aws_cloudwatch_log_group.frontend_log_group.name
+  log_group_name = aws_cloudwatch_log_group.frontend.name
 
   metric_transformation {
     name          = "radius-cannot-connect-to-api"
