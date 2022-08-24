a3c47af65154cd1c5369e63e5019c586459efe45
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2022-08-24 08:32:12 +0100

    Remove the now unused us_east_1 pagerduty module
    This is now unused as the Route53 healthchecks for frontend have been
    replaced by target group based health checks.

--- govwifi/wifi-london/outputs.tf
+++ govwifi/wifi-london/outputs.tf
@@ -2,10 +2,6 @@ output "replica_admin_app_data_s3_bucket_name" {
   value = module.govwifi_admin.replica_app_data_s3_bucket_name
 }
 
-output "us_east_1_pagerduty_topic_arn" {
-  value = module.us_east_1_pagerduty.topic_arn
-}
-
 output "backend_vpc_id" {
   value = module.backend.backend_vpc_id
 }
