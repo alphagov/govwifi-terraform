d0e02c7a6eb9361087516afaf6d69eae1d6e3f3e
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2022-08-30 10:45:31 +0100

    Add VPC endpoints to the backend VPC
    This will enable switching the apps in the backend VPC to Fargate
    platform version 1.4.

--- govwifi-backend/backend.tf
+++ govwifi-backend/backend.tf
@@ -135,3 +135,97 @@ resource "aws_cloudwatch_log_group" "database_backup_log_group" {
   name              = "${var.env_name}-database-backup-log-group"
   retention_in_days = 90
 }
+
+# VPC Endpoints
+
+resource "aws_vpc_endpoint" "ecr_dkr" {
+  vpc_id            = aws_vpc.wifi_backend.id
+  service_name      = "com.amazonaws.${var.aws_region}.ecr.dkr"
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [
+    aws_security_group.vpc_endpoints.id,
+  ]
+
+  subnet_ids = [for subnet in aws_subnet.wifi_backend_subnet : subnet.id]
+
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "ecr_api" {
+  vpc_id            = aws_vpc.wifi_backend.id
+  service_name      = "com.amazonaws.${var.aws_region}.ecr.api"
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [
+    aws_security_group.vpc_endpoints.id,
+  ]
+
+  subnet_ids = [for subnet in aws_subnet.wifi_backend_subnet : subnet.id]
+
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.wifi_backend.id
+  service_name      = "com.amazonaws.${var.aws_region}.s3"
+  vpc_endpoint_type = "Gateway"
+
+  route_table_ids = [aws_vpc.wifi_backend.main_route_table_id]
+}
+
+resource "aws_vpc_endpoint" "secretsmanager" {
+  vpc_id            = aws_vpc.wifi_backend.id
+  service_name      = "com.amazonaws.${var.aws_region}.secretsmanager"
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [
+    aws_security_group.vpc_endpoints.id,
+  ]
+
+  subnet_ids = [for subnet in aws_subnet.wifi_backend_subnet : subnet.id]
+
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "logs" {
+  vpc_id            = aws_vpc.wifi_backend.id
+  service_name      = "com.amazonaws.${var.aws_region}.logs"
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [
+    aws_security_group.vpc_endpoints.id,
+  ]
+
+  subnet_ids = [for subnet in aws_subnet.wifi_backend_subnet : subnet.id]
+
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id            = aws_vpc.wifi_backend.id
+  service_name      = "com.amazonaws.${var.aws_region}.ssm"
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [
+    aws_security_group.vpc_endpoints.id,
+  ]
+
+  subnet_ids = [for subnet in aws_subnet.wifi_backend_subnet : subnet.id]
+
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "ssmmessages" {
+  vpc_id            = aws_vpc.wifi_backend.id
+  service_name      = "com.amazonaws.${var.aws_region}.ssmmessages"
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [
+    aws_security_group.vpc_endpoints.id,
+  ]
+
+  subnet_ids = [for subnet in aws_subnet.wifi_backend_subnet : subnet.id]
+
+  private_dns_enabled = true
+}
