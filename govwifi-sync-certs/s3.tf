c3c1cc32973a2dae8a60c64a743e02fbf17670ad
Sebastian Szypowicz <sebastian.szypowicz@digital.cabinet-office.gov.uk>
2023-05-23 15:39:19 +0100

    A fix for Cert Sync job, ACLs need to be removed on the bucket.

--- govwifi-sync-certs/s3.tf
+++ govwifi-sync-certs/s3.tf
@@ -2,11 +2,6 @@ resource "aws_s3_bucket" "sync_certs_bucket" {
   bucket = "govwifi-sync-certs-logs-${var.env}-${lower(var.region_name)}"
 }
 
-resource "aws_s3_bucket_acl" "sync_certs_bucket_acl" {
-  bucket = aws_s3_bucket.sync_certs_bucket.id
-  acl    = "private"
-}
-
 resource "aws_s3_bucket_public_access_block" "sync_certs_bucket_block" {
   bucket = aws_s3_bucket.sync_certs_bucket.id
 
