7fc862b8da018ad16a5f13c8a99eecad7a19f754
Camille Descartes <camille.descartes@digital.cabinet-office.gov.uk>
2023-08-08 16:51:17 +0100

    Add Scheduled Task To Run Admin Database Migrations

--- govwifi-admin/scheduled-tasks.tf
+++ govwifi-admin/scheduled-tasks.tf
@@ -191,3 +191,50 @@ resource "aws_cloudwatch_event_target" "admin_backup_service_emails" {
 EOF
 
 }
+
+# rake: run admin database migrations
+
+resource "aws_cloudwatch_event_rule" "run_admin_migrations" {
+  name                = "${var.env_name}-run-admin-migrations"
+  description         = "Performs rake database migration on admin database"
+  schedule_expression = "cron(0 11 * * ? *)"
+  is_enabled          = false
+}
+
+
+resource "aws_cloudwatch_event_target" "run_admin_migrations" {
+  target_id = "${var.env_name}-run-admin-migrations"
+  arn       = aws_ecs_cluster.admin_cluster.arn
+  rule      = aws_cloudwatch_event_rule.run_admin_migrations.name
+  role_arn  = aws_iam_role.scheduled_task.arn
+
+  ecs_target {
+    task_count          = 1
+    task_definition_arn = aws_ecs_task_definition.admin_task.arn
+    launch_type         = "FARGATE"
+    platform_version    = "1.4.0"
+
+    network_configuration {
+      subnets = var.subnet_ids
+
+      security_groups = concat(
+        [aws_security_group.admin_ec2_in.id],
+        [aws_security_group.admin_ec2_out.id]
+      )
+
+      assign_public_ip = true
+    }
+  }
+
+  input = <<EOF
+{
+  "containerOverrides": [
+    {
+      "name": "admin",
+      "command": ["bundle", "exec", "rails", "db:migrate"]
+    }
+  ]
+}
+EOF
+
+}
