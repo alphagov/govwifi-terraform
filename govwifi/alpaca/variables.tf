6f50c05628dfb04c8878897a6ad29a0cac2bb4d2
leonaAtkins <leona.atkins@digital.cabinet-office.gov.uk>
2023-08-24 10:21:24 +0100

    feat: [GW-1057] update SSH Keys

--- govwifi/alpaca/variables.tf
+++ govwifi/alpaca/variables.tf
@@ -1,6 +1,11 @@
 variable "ssh_key_name" {
   type    = string
-  default = "govwifi-alpaca-bastion-20230120"
+  default = "govwifi-alpaca-ec2-instances-key-20230822"
+}
+
+variable "ssh_key_name_pub" {
+  type = string
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC4q8NhyH2wtEj8CiqOn+4OFgrB6wZgdzB/qjEh1t7ATQwgkFii0vtsJIzTfOVyxLr0rF79TQCdy7RcVidnZJOoa6QYJRDUx61f2bSacDsiI04/6QSAzqYe0x12fDoRMZqU6GWN1tRY3HYGdLCSMo8QaYonpdiyuS2q+gzFl1V1pk2c3/VT0KhZMWPV1qh6y6uCV13CkiFEgRpqhWTxfagv38lCRFvgJVmNxXtwBME8lqAs7DRoxfD5WZ4oGWO+wCaw+3QgD6gGDMUxpLk1CtL0BVpZ73OGB1XW2atTf2Ugma1jLMvP5IIrEDhMfOEX4iFexRVZBTqIqmsFaHjTH6BRp0J5FJ4suDVIv9eMblgZEGomDmP/T3ZIUq+96Z5BcOLpW1dEdCswbXtzuw7F+hIceSbnYMefSJ/8mhjxTcvBrJK4pv/BKEx/1UBOYtcwu3PZ52oiQaFFxElJiFTa/SUTLAks50e90o7kIZo2z4eal2e5mvWLSNwjA5kCniATfTs= govwifi-developers@digital.cabinet-office.gov.uk"
 }
 
 variable "notify_ips" {
