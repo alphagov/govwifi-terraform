2c2324716c5383f6a34743dcb3043e7a976e389b
leonaAtkins <leona.atkins@digital.cabinet-office.gov.uk>
2023-09-19 18:40:15 +0100

    fix: [GW-1125] revert smoke test creds

--- govwifi-smoke-tests/locals.tf
+++ govwifi-smoke-tests/locals.tf
@@ -1,4 +1,5 @@
+## Relates to the Smoke Test Notify Account!
 locals {
-  notify_go_template_id  = "8304abb4-e829-442d-a06e-2ae485f5dd5a"
-  smoketest_phone_number = "+447860003687"
+  notify_go_template_id  = "ab004ebd-c74f-43e3-95bc-60c9bd325a83"
+  smoketest_phone_number = "+447860003343"
 }
