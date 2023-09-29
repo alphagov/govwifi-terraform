035482e7ebdfcaf939eea9e7087813795395f294
Camille Descartes <camille.descartes@digital.cabinet-office.gov.uk>
2023-09-29 11:26:22 +0100

    Add database parameter group for mysql 8 admin database
    We are upgrading the admin database to Mysql 8. Making database parameter
    groups compatible.

--- govwifi-admin/db.tf
+++ govwifi-admin/db.tf
@@ -28,6 +28,40 @@ resource "aws_db_parameter_group" "db_parameters" {
   }
 }
 
+
+
+resource "aws_db_parameter_group" "db_parameters_v8" {
+  name        = "${var.env_name}-mysql8-admin-db-parameter-group"
+  family      = "mysql8.0"
+  description = "DB parameter configuration for govwifi-admin"
+
+  parameter {
+    name  = "slow_query_log"
+    value = 1
+  }
+
+  parameter {
+    name  = "general_log"
+    value = 0
+  }
+
+  parameter {
+    name  = "log_queries_not_using_indexes"
+    value = 1
+  }
+
+  parameter {
+    name  = "log_output"
+    value = "FILE"
+  }
+
+  tags = {
+    Name = "${title(var.env_name)} mysql 8 DB parameter group for govwifi-admin"
+  }
+}
+
+
+
 resource "aws_db_option_group" "mariadb_audit" {
   name = "${var.env_name}-admin-db-audit"
 
