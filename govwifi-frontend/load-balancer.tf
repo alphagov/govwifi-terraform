53400d01b90d21dd68c84630ba9e0b29c5bfe763
leonaAtkins <leona.atkins@digital.cabinet-office.gov.uk>
2024-01-11 11:09:30 +0000

    fix: [GW-399] remove redundant code.

--- govwifi-frontend/load-balancer.tf
+++ govwifi-frontend/load-balancer.tf
@@ -46,22 +46,3 @@ resource "aws_lb_target_group" "main" {
     type = "source_ip"
   }
 }
-
-/*
-# TODO These EIPs are being used to test the network load balancer,
-# and can be replaced by the radius_eips once the network load
-# balancer is used behind these eips
-resource "aws_eip" "test_radius_eips" {
-  count = var.radius_instance_count
-  vpc   = true
-
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  tags = {
-    Name   = "${title(var.env_name)} Frontend Radius-${var.dns_numbering_base + count.index + 1}"
-    Region = title(var.aws_region_name)
-  }
-}
-*/
