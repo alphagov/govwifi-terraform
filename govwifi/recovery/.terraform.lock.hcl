c1aa39bfa8d93e12a210b9a55de121d3c3e6b769
leonaAtkins <leona.atkins@digital.cabinet-office.gov.uk>
2024-01-29 14:28:02 +0000

    initial commit for bcp

---
+++ govwifi/recovery/.terraform.lock.hcl
@@ -0,0 +1,65 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/archive" {
+  version = "2.4.0"
+  hashes = [
+    "h1:EtN1lnoHoov3rASpgGmh6zZ/W6aRCTgKC7iMwvFY1yc=",
+    "h1:cJokkjeH1jfpG4QEHdRx0t2j8rr52H33A7C/oX73Ok4=",
+    "zh:18e408596dd53048f7fc8229098d0e3ad940b92036a24287eff63e2caec72594",
+    "zh:392d4216ecd1a1fd933d23f4486b642a8480f934c13e2cae3c13b6b6a7e34a7b",
+    "zh:655dd1fa5ca753a4ace21d0de3792d96fff429445717f2ce31c125d19c38f3ff",
+    "zh:70dae36c176aa2b258331ad366a471176417a94dd3b4985a911b8be9ff842b00",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:7d8c8e3925f1e21daf73f85983894fbe8868e326910e6df3720265bc657b9c9c",
+    "zh:a032ec0f0aee27a789726e348e8ad20778c3a1c9190ef25e7cff602c8d175f44",
+    "zh:b8e50de62ba185745b0fe9713755079ad0e9f7ac8638d204de6762cc36870410",
+    "zh:c8ad0c7697a3d444df21ff97f3473a8604c8639be64afe3f31b8ec7ad7571e18",
+    "zh:df736c5a2a7c3a82c5493665f659437a22f0baf8c2d157e45f4dd7ca40e739fc",
+    "zh:e8ffbf578a0977074f6d08aa8734e36c726e53dc79894cfc4f25fadc4f45f1df",
+    "zh:efea57ff23b141551f92b2699024d356c7ffd1a4ad62931da7ed7a386aef7f1f",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/aws" {
+  version = "5.12.0"
+  hashes = [
+    "h1:3Gchmc4oyFvWh3B4tEOQN5UOCd0bfp0P4fEgdpatmlo=",
+    "h1:i28TUsgqoKs891cyDU0V9fFAwEz/RqbwF8sQShLfNq0=",
+    "zh:0953565eb67ece49556dc9046c77322dc6c76e0ae6fa0c9fd6710b6afa2588c9",
+    "zh:43676f3592c127a971719cc37b9199967376fb05d445b356f1545609e2b84bf8",
+    "zh:46422ab8044b35e90f422ffabc17fa043ec8e4a33e3df2f8b305d63a950c0edb",
+    "zh:4d34f024a82d31d10b5a9498d26fca71e3e35c543dfc5185c94c3205bc4dba22",
+    "zh:51be0eeb882f041fc2679bd621e64cd775d013ae003055cea013c9d630c15dfb",
+    "zh:7ca9252befa7271899febde25b679a73f90dbdb700cbbfec07d29389a3937131",
+    "zh:8325b2152be0534a718e497a3273cf6c42880e78f290dc35024feef2e0af8e97",
+    "zh:98f0c4d4c190cf4897cb9075a538f42f2998566e9f2d15755901fbb4862f8b32",
+    "zh:9b12af85486a96aedd8d7984b0ff811a4b42e3d88dad1a3fb4c0b580d04fa425",
+    "zh:a71e0bc6754bb3924b31727d80f05b04fa65247c009ffdfd2a715a01d95b373d",
+    "zh:a82ae67ce3d4c7aaae761a592275b8cac5e9965a30b2dba951c1d965b3121006",
+    "zh:c5510eca023cec89557a8244648bf8ad9a0cd3189b6abf6dcceba30e3b2e8c6d",
+    "zh:cd11fe9c83793e838b6f90a55840fc45e7c106b358a68f0a88db09a29a321c9a",
+    "zh:e451ad353f219a2922b92e786a93c31658168b896317be127798cddfa9a99363",
+    "zh:e4b70a70e925b9ccb7d44e17fd8e7b89aa744a965f298f8bb2480a5c96f3c4f0",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/random" {
+  version = "3.5.1"
+  hashes = [
+    "h1:IL9mSatmwov+e0+++YX2V6uel+dV6bn+fC/cnGDK3Ck=",
+    "h1:VSnd9ZIPyfKHOObuQCaKfnjIHRtR7qTw19Rz8tJxm+k=",
+    "zh:04e3fbd610cb52c1017d282531364b9c53ef72b6bc533acb2a90671957324a64",
+    "zh:119197103301ebaf7efb91df8f0b6e0dd31e6ff943d231af35ee1831c599188d",
+    "zh:4d2b219d09abf3b1bb4df93d399ed156cadd61f44ad3baf5cf2954df2fba0831",
+    "zh:6130bdde527587bbe2dcaa7150363e96dbc5250ea20154176d82bc69df5d4ce3",
+    "zh:6cc326cd4000f724d3086ee05587e7710f032f94fc9af35e96a386a1c6f2214f",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:b6d88e1d28cf2dfa24e9fdcc3efc77adcdc1c3c3b5c7ce503a423efbdd6de57b",
+    "zh:ba74c592622ecbcef9dc2a4d81ed321c4e44cddf7da799faa324da9bf52a22b2",
+    "zh:c7c5cde98fe4ef1143bd1b3ec5dc04baf0d4cc3ca2c5c7d40d17c0e9b2076865",
+    "zh:dac4bad52c940cd0dfc27893507c1e92393846b024c5a9db159a93c534a3da03",
+    "zh:de8febe2a2acd9ac454b844a4106ed295ae9520ef54dc8ed2faf29f12716b602",
+    "zh:eab0d0495e7e711cca367f7d4df6e322e6c562fc52151ec931176115b83ed014",
+  ]
+}
---
+++ govwifi/recovery/london.tf
@@ -0,0 +1,448 @@
+locals {
+  london_aws_region      = "eu-west-2"
+  london_aws_region_name = "London"
+}
+
+provider "aws" {
+  alias  = "london"
+  region = local.london_aws_region
+
+  default_tags {
+    tags = {
+      Environment = title(local.env_name)
+    }
+  }
+  /* As tags are computed, terraform always thinks have checked, re: issue https://github.com/hashicorp/terraform-provider-aws/issues/18311#issuecomment-1544330448 */
+  ignore_tags {
+    keys = ["Environment", "Staging"]
+  }
+}
+
+module "london_keys" {
+  providers = {
+    aws = aws.london
+  }
+
+  source = "../../govwifi-keys"
+
+  govwifi_bastion_key_name = "recovery-bastion-20200717"
+  govwifi_bastion_key_pub  = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDL5wGVJ8aXL0QUhIvfLV2BMLC9Tk74jnChC40R9ipzK0AuatcaXdj0PEm8sh8sHlXEmdmVDq/4s8XaEkF7MDl38qbjxxHRTpCgcTrYzJGad3xgr1+zhpD8Kfnepex/2pR7z7kOCv7EDx4vRTc8vu1ttcmJiniBmgjc1xVk1A5aB72GxffZrow7B0iopP16vEPvllUjsDoOaeLJukDzsbZaP2RRYBqIA4qXunfJpuuu/o+T+YR4LkTB+9UBOOGrX50T80oTtJMKD9ndQ9CC9sqlrOzE9GiZz9db7D9iOzIZoTT6dBbgEOfCGmkj7WS2NjF+D/pEN/edkIuNGvE+J/HqQ179Xm/VCx5Kr6ARG+xk9cssCQbEFwR46yitaPA7B4mEiyD9XvUW2tUeVKdX5ybUFqV++2c5rxTczuH4gGlEGixIqPeltRvkVrN6qxnrbDAXE2bXymcnEN6BshwGKR+3OUKTS8c53eWmwiol6xwCp8VUI8/66tC/bCTmeur07z2LfQsIo745GzPuinWfUm8yPkZOD3LptkukO1aIfgvuNmlUKTwKSLIIwwsqTZ2FcK39A8g3Iq3HRV+4JwOowLJcylRa3QcSH9wdjd69SqPrZb0RhW0BN1mTX2tEBl1ryUUpKsqpMbvjl28tn6MGsU/sRhBLqliduOukGubD29LlAQ== "
+
+  create_production_bastion_key = 0
+
+  govwifi_key_name     = var.ssh_key_name
+  govwifi_key_name_pub = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDOxYtGJARr+ZUB9wMWMX/H+myTidFKx+qcBsXuri5zavQ6K4c0WhSkypXfET9BBtC1ZU77B98mftegxKdKcKmFbCVlv39pIX+xj2vjuCzHlzezI1vB4mdAXNhc8b4ArvFJ8lG2GLa1ZD8H/8akpv6EcplwyUv6ZgQMPl6wfMF6d0Qe/eOJ/bV570icX9NYLGkdLRbudkRc12krt6h451qp1vO7f2FQOnPR2cnyLGd/FxhrmAOqJsDk9CRNSwHJe1lsSCz6TkQk1bfCTxZ7g2hWSNRBdWPj0RJbbezy3X3/pz4cFL8mCC1esJ+nptUZ7CXeyirtCObIepniXIItwtdIVqixaMSjfagUGd0L1zFEVuH0bct3mh3u3TyVbNHP4o4pFHvG0sm5R1iDB8/xe2NJdxmAsn3JqeXdsQ6uI/oz31OueFRPyZI0VeDw7B4bhBMZ0w/ncrYJ9jFjfPvzhAVZgQX5Pxtp5MUCeU9+xIdAN2bESmIvaoSEwno7WJ4z61d83pLMFUuS9vNRW4ykgd1BzatLYSkLp/fn/wYNn6DBk7Da6Vs1Y/jgkiDJPGeFlEhW3rqOjTKrpKJBw6LBsMyI0BtkKoPoUTDlKSEX5JlNWBX2z5eSEhe+WEQjc4ZnbLUOKRB5+xNOGahVyk7/VF8ZaZ3/GXWY7MEfZ8TIBBcAjw== GovWifi-DevOps@digital.cabinet-office.gov.uk"
+
+}
+
+module "london_backend" {
+  providers = {
+    aws = aws.london
+  }
+
+  source        = "../../govwifi-backend"
+  env           = "recovery"
+  env_name      = local.env_name
+  env_subdomain = local.env_subdomain
+
+  # AWS VPC setup -----------------------------------------
+  aws_region      = local.london_aws_region
+  route53_zone_id = data.aws_route53_zone.main.zone_id
+  aws_region_name = local.london_aws_region_name
+  vpc_cidr_block  = "10.106.0.0/16"
+
+  administrator_cidrs = var.administrator_cidrs
+  frontend_radius_ips = local.frontend_radius_ips
+
+  bastion_instance_type     = "t2.micro"
+  bastion_ssh_key_name      = "recovery-bastion-20200717"
+  enable_bastion_monitoring = false
+  aws_account_id            = local.aws_account_id
+  db_instance_count         = 1
+  session_db_instance_type  = "db.t3.small"
+  session_db_storage_gb     = 20
+  db_backup_retention_days  = 1
+  db_encrypt_at_rest        = true
+  db_maintenance_window     = "sat:01:42-sat:02:12"
+  db_backup_window          = "04:42-05:42"
+  db_replica_count          = 0
+  rr_instance_type          = "db.t3.large"
+  rr_storage_gb             = 200
+  # TODO This should happen inside the module
+  user_rr_hostname           = "users-rr.${lower(local.london_aws_region_name)}.${local.env_subdomain}.service.gov.uk"
+  critical_notifications_arn = module.london_notifications.topic_arn
+  capacity_notifications_arn = module.london_notifications.topic_arn
+
+  # Seconds. Set to zero to disable monitoring
+  db_monitoring_interval = 60
+
+  # Passed to application
+  # TODO This should happen inside the module
+  user_db_hostname      = "users-db.${lower(local.london_aws_region_name)}.${local.env_subdomain}.service.gov.uk"
+  user_db_instance_type = "db.t3.small"
+  user_db_storage_gb    = 20
+
+  prometheus_ip_london  = module.london_prometheus.eip_public_ip
+  prometheus_ip_ireland = module.dublin_prometheus.eip_public_ip
+  grafana_ip            = module.london_grafana.eip_public_ip
+
+  backup_mysql_rds = local.backup_mysql_rds
+
+  db_storage_alarm_threshold = 19327342936
+}
+
+module "london_frontend" {
+  providers = {
+    aws           = aws.london
+    aws.us_east_1 = aws.us_east_1
+  }
+
+  source        = "../../govwifi-frontend"
+  env_name      = local.env_name
+  env_subdomain = local.env_subdomain
+  env           = local.env
+
+  # AWS VPC setup -----------------------------------------
+  # LONDON
+  aws_region = local.london_aws_region
+
+  aws_region_name    = local.london_aws_region_name
+  route53_zone_id    = data.aws_route53_zone.main.zone_id
+  vpc_cidr_block     = "10.102.0.0/16"
+  rack_env           = "recovery"
+  sentry_current_env = "recovery"
+
+  backend_vpc_id = module.london_backend.backend_vpc_id
+
+  # Instance-specific setup -------------------------------
+  radius_instance_count      = 3
+  enable_detailed_monitoring = false
+
+  # eg. dns records are generated for radius(N).x.service.gov.uk
+  # where N = this base + 1 + server#
+  dns_numbering_base = 3
+
+  ami                   = "ami-2218f945"
+  ssh_key_name          = var.ssh_key_name
+  frontend_docker_image = format("%s/frontend:recovery", local.docker_image_path)
+  raddb_docker_image    = format("%s/raddb:recovery", local.docker_image_path)
+  create_ecr            = 1
+
+  admin_app_data_s3_bucket_name = module.london_admin.app_data_s3_bucket_name
+
+  logging_api_base_url = module.london_api.api_base_url
+  auth_api_base_url    = module.london_api.api_base_url
+
+  authentication_api_internal_dns_name = module.london_api.authentication_api_internal_dns_name
+  logging_api_internal_dns_name        = one(module.london_api.logging_api_internal_dns_name)
+
+  notification_arn           = module.london_notifications.topic_arn
+  critical_notifications_arn = module.london_notifications.topic_arn
+
+  bastion_server_ip = module.london_backend.bastion_public_ip
+
+  prometheus_ip_london  = module.london_prometheus.eip_public_ip
+  prometheus_ip_ireland = module.london_prometheus.eip_public_ip
+
+  prometheus_security_group_id = module.london_prometheus.prometheus_security_group_id
+
+  radius_cidr_blocks = [for ip in local.frontend_radius_ips : "${ip}/32"]
+
+  london_backend_vpc_cidr = module.london_backend.vpc_cidr_block
+}
+
+module "london_admin" {
+  providers = {
+    aws             = aws.london
+    aws.replication = aws.dublin
+  }
+
+  source        = "../../govwifi-admin"
+  env_name      = local.env_name
+  env_subdomain = local.env_subdomain
+  env           = local.env
+
+  aws_region      = local.london_aws_region
+  aws_region_name = local.london_aws_region_name
+  vpc_id          = module.london_backend.backend_vpc_id
+  instance_count  = 1
+
+  vpc_endpoints_security_group_id = module.london_backend.vpc_endpoints_security_group_id
+
+  route53_zone_id = data.aws_route53_zone.main.zone_id
+
+  admin_docker_image   = format("%s/admin:recovery", local.docker_image_path)
+  rails_env            = "recovery"
+  app_env              = "recovery"
+  sentry_current_env   = "recovery"
+  ecr_repository_count = 1
+
+  subnet_ids = module.london_backend.backend_subnet_ids
+
+  db_instance_type         = "db.t3.medium"
+  db_storage_gb            = 120
+  db_backup_retention_days = 1
+  db_encrypt_at_rest       = true
+  db_maintenance_window    = "sat:00:42-sat:01:12"
+  db_backup_window         = "03:42-04:42"
+  db_monitoring_interval   = 60
+
+  rr_db_host = "db.london.recovery.wifi.service.gov.uk"
+  rr_db_name = "govwifi_recovery"
+
+  user_db_host = "users-db.${lower(local.london_aws_region_name)}.${local.env_subdomain}.service.gov.uk"
+  user_db_name = "govwifi_recovery_users"
+
+  critical_notifications_arn = module.london_notifications.topic_arn
+  capacity_notifications_arn = module.london_notifications.topic_arn
+
+  rds_monitoring_role = module.london_backend.rds_monitoring_role
+
+  london_radius_ip_addresses = module.london_frontend.eip_public_ips
+  dublin_radius_ip_addresses = module.dublin_frontend.eip_public_ips
+  logging_api_search_url     = "https://api-elb.london.${local.env_subdomain}.service.gov.uk:8443/logging/authentication/events/search/"
+  public_google_api_key      = var.public_google_api_key
+
+  zendesk_api_endpoint = "https://govuk.zendesk.com/api/v2/"
+  zendesk_api_user     = var.zendesk_api_user
+
+  bastion_server_ip = module.london_backend.bastion_public_ip
+
+  notification_arn = module.london_notifications.topic_arn
+
+  elasticsearch_endpoint = module.london_elasticsearch.endpoint
+}
+
+module "london_api" {
+  providers = {
+    aws = aws.london
+  }
+
+  source        = "../../govwifi-api"
+  env           = "recovery"
+  env_name      = "recovery"
+  env_subdomain = local.env_subdomain
+
+  backend_elb_count      = 1
+  backend_instance_count = 2
+  aws_account_id         = local.aws_account_id
+  aws_region_name        = local.london_aws_region_name
+  aws_region             = local.london_aws_region
+  route53_zone_id        = data.aws_route53_zone.main.zone_id
+  vpc_id                 = module.london_backend.backend_vpc_id
+  safe_restart_enabled   = 1
+
+  vpc_endpoints_security_group_id = module.london_backend.vpc_endpoints_security_group_id
+
+  capacity_notifications_arn = module.london_notifications.topic_arn
+  devops_notifications_arn   = module.london_notifications.topic_arn
+  notification_arn           = module.london_notifications.topic_arn
+
+  user_signup_docker_image      = format("%s/user-signup-api:recovery", local.docker_image_path)
+  logging_docker_image          = format("%s/logging-api:recovery", local.docker_image_path)
+  safe_restart_docker_image     = format("%s/safe-restarter:recovery", local.docker_image_path)
+  backup_rds_to_s3_docker_image = format("%s/database-backup:recovery", local.docker_image_path)
+
+  create_wordlist_bucket = true
+  wordlist_file_path     = "../wordlist-short"
+  ecr_repository_count   = 1
+
+  db_hostname = "db.${lower(local.london_aws_region_name)}.${local.env_subdomain}.service.gov.uk"
+
+  user_db_hostname = "users-db.${lower(local.london_aws_region_name)}.${local.env_subdomain}.service.gov.uk"
+  user_rr_hostname = "users-db.${lower(local.london_aws_region_name)}.${local.env_subdomain}.service.gov.uk"
+
+  rack_env                  = "recovery"
+  app_env                   = "recovery"
+  sentry_current_env        = "recovery"
+  radius_server_ips         = local.frontend_radius_ips
+  subnet_ids                = module.london_backend.backend_subnet_ids
+  private_subnet_ids        = module.london_backend.backend_private_subnet_ids
+  nat_gateway_elastic_ips   = module.london_backend.nat_gateway_elastic_ips
+  notify_ips                = var.notify_ips
+  user_signup_api_is_public = 1
+
+  admin_app_data_s3_bucket_name = module.london_admin.app_data_s3_bucket_name
+
+  backend_sg_list = [
+    module.london_backend.be_admin_in,
+  ]
+
+  metrics_bucket_name     = module.london_dashboard.metrics_bucket_name
+  export_data_bucket_name = module.london_dashboard.export_data_bucket_name
+
+  rds_mysql_backup_bucket = module.london_backend.rds_mysql_backup_bucket
+  backup_mysql_rds        = local.backup_mysql_rds
+
+  alb_permitted_security_groups = [
+    module.london_frontend.load_balanced_frontend_service_security_group_id
+  ]
+
+  alb_permitted_cidr_blocks = [
+    local.dublin_frontend_vpc_cidr_block
+  ]
+
+  low_cpu_threshold = 0.3
+
+  elasticsearch_endpoint = module.london_elasticsearch.endpoint
+  smoke_test_ips         = module.london_smoke_tests.eip_public_ips
+}
+
+module "london_route53_notifications" {
+  providers = {
+    aws = aws.us_east_1
+  }
+
+  source = "../../sns-notification"
+
+  topic_name = "govwifi-recovery-london"
+  emails     = [var.notification_email]
+}
+
+module "london_notifications" {
+  providers = {
+    aws = aws.london
+  }
+
+  source = "../../sns-notification"
+
+  topic_name = "govwifi-recovery"
+  emails     = [var.notification_email]
+}
+
+module "london_dashboard" {
+  providers = {
+    aws = aws.london
+  }
+
+  source   = "../../govwifi-dashboard"
+  env_name = local.env_name
+}
+
+module "london_prometheus" {
+  providers = {
+    aws = aws.london
+  }
+
+  source          = "../../govwifi-prometheus"
+  env_name        = local.env_name
+  aws_region      = local.london_aws_region
+  aws_region_name = local.london_aws_region_name
+  aws_account_id  = local.aws_account_id
+
+
+  ssh_key_name = var.ssh_key_name
+
+  frontend_vpc_id = module.london_frontend.frontend_vpc_id
+
+  fe_admin_in = module.london_frontend.fe_admin_in
+
+  wifi_frontend_subnet       = module.london_frontend.frontend_subnet_id
+  london_radius_ip_addresses = module.london_frontend.eip_public_ips
+  dublin_radius_ip_addresses = module.dublin_frontend.eip_public_ips
+
+  grafana_ip = module.london_grafana.eip_public_ip
+}
+
+module "london_grafana" {
+  providers = {
+    aws = aws.london
+  }
+
+  source                     = "../../govwifi-grafana"
+  env_name                   = local.env_name
+  env_subdomain              = local.env_subdomain
+  aws_region                 = local.london_aws_region
+  aws_region_name            = local.london_aws_region_name
+  critical_notifications_arn = module.london_notifications.topic_arn
+
+  route53_zone_id = data.aws_route53_zone.main.zone_id
+
+  ssh_key_name = var.ssh_key_name
+
+  subnet_ids         = module.london_backend.backend_subnet_ids
+  backend_subnet_ids = module.london_backend.backend_subnet_ids
+  be_admin_in        = module.london_backend.be_admin_in
+
+  vpc_id = module.london_backend.backend_vpc_id
+
+  bastion_ip = module.london_backend.bastion_public_ip
+
+  administrator_cidrs = var.administrator_cidrs
+  prometheus_ips = [
+    module.london_prometheus.eip_public_ip,
+    module.dublin_prometheus.eip_public_ip
+  ]
+  aws_account_id = local.aws_account_id
+
+}
+
+module "london_elasticsearch" {
+  providers = {
+    aws = aws.london
+  }
+
+  source         = "../../govwifi-elasticsearch"
+  domain_name    = "${local.env_name}-elasticsearch"
+  env_name       = local.env_name
+  aws_region     = local.london_aws_region
+  aws_account_id = local.aws_account_id
+  vpc_id         = module.london_backend.backend_vpc_id
+  vpc_cidr_block = module.london_backend.vpc_cidr_block
+
+  backend_subnet_id = module.london_backend.backend_subnet_ids[0]
+}
+
+module "london_smoke_tests" {
+  providers = {
+    aws        = aws.london
+    aws.dublin = aws.dublin
+  }
+
+  source = "../../govwifi-smoke-tests"
+
+  aws_account_id             = local.aws_account_id
+  env_subdomain              = local.env_subdomain
+  env                        = local.env_name
+  smoketests_vpc_cidr        = var.smoketests_vpc_cidr
+  smoketest_subnet_private_a = var.smoketest_subnet_private_a
+  smoketest_subnet_private_b = var.smoketest_subnet_private_b
+  smoketest_subnet_public_a  = var.smoketest_subnet_public_a
+  smoketest_subnet_public_b  = var.smoketest_subnet_public_b
+  aws_region                 = local.london_aws_region
+  create_slack_alert         = 0
+  govwifi_phone_number       = "+447537417039"
+  notify_field               = "govwifirecovery"
+
+  depends_on = [
+    module.london_frontend
+  ]
+}
+
+module "london_sync_certs" {
+  providers = {
+    aws = aws.london
+  }
+
+  source = "../../govwifi-sync-certs"
+
+  env            = local.env
+  aws_account_id = local.aws_account_id
+  aws_region     = local.london_aws_region
+  region_name    = local.london_aws_region_name
+}
+
+module "london_account_policy" {
+  providers = {
+    aws = aws.london
+  }
+
+  source = "../../govwifi-account-policy"
+
+  aws_region     = local.london_aws_region
+  env            = local.env
+  aws_account_id = local.aws_account_id
+  region_name    = local.london_aws_region_name
+
+}
+
