5270f3f08359ef4433381fd7d8cf93e1bff3a6c1
leonaAtkins <leona.atkins@digital.cabinet-office.gov.uk>
2024-08-22 12:20:06 +0100

    feature: [GW-1865] adding SSM to Bastion

--- govwifi-backend/management.tf
+++ govwifi-backend/management.tf
@@ -176,6 +176,12 @@ resource "aws_iam_role" "bastion_instance_role" {
 EOF
 }
 
+resource aws_iam_role_policy_attachment "bastion_instance_ssm" {
+    count      = var.enable_bastion
+    policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+    role = aws_iam_role.bastion_instance_role[0].id
+}
+
 resource "aws_iam_role_policy" "bastion_instance_policy" {
   count      = var.enable_bastion
   name       = "${var.aws_region_name}-${var.env_name}-backend-bastion-instance-policy"
