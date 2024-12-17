9165af81b6c37e0f5c8f7beb486c2b845cbe7199
Camille Descartes <camille.descartes@digital.cabinet-office.gov.uk>
2024-12-17 18:58:13 +0000

    Update Codebuild Image For Acceptance Tests
    Also update address of github repo. We are migrating our code to
    our own github org

--- govwifi-deploy/codebuild-acceptance-tests.tf
+++ govwifi-deploy/codebuild-acceptance-tests.tf
@@ -11,7 +11,7 @@ resource "aws_codebuild_project" "govwifi_codebuild_acceptance_tests" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:6.0"
+    image                       = "aws/codebuild/standard:7.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
     privileged_mode             = true
